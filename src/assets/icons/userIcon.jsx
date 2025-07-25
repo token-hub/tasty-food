fe3f678a28b9c1e77d74c7990cd2a2c33e15dbcb
John <johnsuyang2118@gmail.com>
2025-07-26 06:11:02 +0800

    Added user and search icons

---
+++ src/assets/icons/userIcon.jsx
@@ -0,0 +1,20 @@
+function UserIcon({ className, height = "16", width = "16" }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            fill="currentColor"
+            className={`bi bi-person-circle ${className}`}
+            viewBox="0 0 16 16"
+        >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+            <path
+                fill-rule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+            />
+        </svg>
+    );
+}
+
+export default UserIcon;
