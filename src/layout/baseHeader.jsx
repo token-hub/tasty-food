eb4629ad7feba6947737ec7dfb8e1427bf05388a
John <johnsuyang2118@gmail.com>
2025-07-26 06:35:50 +0800

    Finished base header

--- src/layout/baseHeader.jsx
+++ src/layout/baseHeader.jsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router";
+import UserIcon from "../assets/icons/userIcon";
+import SearchBar from "../components/searchBar";
+import SignInIcon from "../assets/icons/signInIcon";
+import { isAuthenticated } from "../lib/constants";
+
 function BaseHeader() {
-    return <div>BaseHeader</div>;
+    return (
+        <>
+            <nav className="navbar navbar-light bg-primary">
+                <div className="container my-1">
+                    <div className="d-flex align-items-center w-100">
+                        <Link to="/" className="navbar-brand text-white fs-4 flex-grow-1 flex-sm-grow-0">
+                            Tasty Food
+                        </Link>
+                        <div className="d-none d-sm-flex flex-grow-1 ">
+                            <SearchBar />
+                        </div>
+                        {isAuthenticated ? (
+                            <>
+                                <span className="text-white fs-6 me-2">John</span> <UserIcon className="text-white" height="20" width="20" />
+                            </>
+                        ) : (
+                            <>
+                                <span className="text-white fs-6 me-2">Sign In</span> <SignInIcon className="text-white" height="20" width="20" />
+                            </>
+                        )}
+                    </div>
+                    <div className="d-flex d-sm-none w-100 mt-2">
+                        <SearchBar inputWidth="w-100" />
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default BaseHeader;
