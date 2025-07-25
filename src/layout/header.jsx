452f9caf891f3ef63747aa2bd7887299f48c67aa
John <johnsuyang2118@gmail.com>
2025-07-25 12:35:04 +0800

    initial header component with link

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,20 +1,13 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 function Header() {
     return (
         <>
             <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img
-                            src="/docs/5.0/assets/brand/bootstrap-logo.svg"
-                            alt=""
-                            width={30}
-                            height={24}
-                            className="d-inline-block align-text-top"
-                        />
-                        Bootstrap
-                    </a>
+                <div className="container">
+                    <Link to="/login" className="navbar-brand text-primary">
+                        Tasty Food
+                    </Link>
                 </div>
             </nav>
 
