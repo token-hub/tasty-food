6cf73b39cd096dd70bb53765d651db0ab9e7e174
John <johnsuyang2118@gmail.com>
2025-07-26 06:23:56 +0800

    Make search bar reusable

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,15 @@
+import SearchIcon from "../assets/icons/searchIcon";
+
+function SearchBar({ inputWidth = "w-50" }) {
+    return (
+        <form className="d-flex justify-content-center w-100  " role="search">
+            <input className={`form-control me-2 ${inputWidth} text-muted`} type="search" placeholder="Search" aria-label="Search recipes" />
+
+            <button className="btn ms-n6 no-btn-outline search-btn">
+                <SearchIcon className="text-primary " height="20" width="20" />
+            </button>
+        </form>
+    );
+}
+
+export default SearchBar;
