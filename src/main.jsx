56c64c27ca4587cbfd3155ddbb35fd56ab8bfe3e
John <johnsuyang2118@gmail.com>
2025-07-25 09:08:50 +0800

    add boostrap to the app so that bootsrap js will work on modals

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import "./scss/style.scss";
-// import * as bootstrap from "bootstrap";
+import * as bootstrap from "bootstrap";
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
