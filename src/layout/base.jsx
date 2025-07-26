c738b3ff82187accb91eb7641f6cd6bff343bdf9
John <johnsuyang2118@gmail.com>
2025-07-27 07:57:01 +0800

    adjust space of sidebar take on different screen sizes

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -7,12 +7,14 @@ function Base() {
         <>
             <BaseHeader />
             <div className="container">
-                <div className="row mt-3">
-                    <div className="col-md-2">
-                        <Sidebar />
-                    </div>
-                    <div className="col-md-10">
-                        <Outlet />
+                <div className="d-none d-md-block">
+                    <div className="row mt-3 ">
+                        <div className="col-md-3 col-lg-2">
+                            <Sidebar />
+                        </div>
+                        <div className="col-md-9 col-lg-10">
+                            <Outlet />
+                        </div>
                     </div>
                 </div>
             </div>
