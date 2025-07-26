56a1c42320853ce9886bac00ddb56fc628b9092d
John <johnsuyang2118@gmail.com>
2025-07-26 11:06:28 +0800

    Added the sidebar

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router";
 import BaseHeader from "./baseHeader";
+import Sidebar from "../components/sidebar/sidebar";
 
 function Base() {
     return (
         <>
             <BaseHeader />
-            <Outlet />
+            <div className="container">
+                <div className="row mt-3">
+                    <div className="col-md-2">
+                        <Sidebar />
+                    </div>
+                    <div className="col-md-10">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
