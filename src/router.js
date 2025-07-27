719a8acc81c691b7d658ca086099ac8f9330e894
John <johnsuyang2118@gmail.com>
2025-07-28 05:16:29 +0800

    Create the me path to show the layout that contains the sidebar

--- src/router.js
+++ src/router.js
@@ -3,12 +3,18 @@ import Recipes from "./pages/recipes";
 
 import AuthLayout from "./layout/authLayout";
 import BaseLayout from "./layout/base";
+import MyAccountLayout from "./layout/myAccountLayout";
 import { createBrowserRouter } from "react-router";
 
 const router = createBrowserRouter([
     {
         path: "/",
         Component: BaseLayout,
+        children: [{ index: true, Component: Recipes }]
+    },
+    {
+        path: "/me",
+        Component: MyAccountLayout,
         children: [{ index: true, Component: Recipes }]
     },
     {
