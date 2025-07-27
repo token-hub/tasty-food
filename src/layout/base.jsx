5060ded0f101d078e562ca236bd9f2d6682d73c1
John <johnsuyang2118@gmail.com>
2025-07-28 05:16:04 +0800

    Remove the sidebar component in base layout because it should only show when the user is visiting its own account

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -10,9 +10,7 @@ function Base() {
             <div className="container">
                 <div className="d-none d-md-block">
                     <div className="row mt-3 ">
-                        <div className="col-md-3 col-lg-2">
-                            <Sidebar />
-                        </div>
+                        <div className="col-md-3 col-lg-2">{/* <Sidebar /> */}</div>
                         <div className="col-md-9 col-lg-10">
                             <Outlet />
                         </div>
