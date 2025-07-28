ba3ea2a14470257b3ab9bdf3166b846f8df1aa2f
John <johnsuyang2118@gmail.com>
2025-07-29 07:54:01 +0800

    Change the main header to not bolded

--- src/components/main/mainHeader.jsx
+++ src/components/main/mainHeader.jsx
@@ -10,7 +10,7 @@ function MainHeader({ text }) {
                     <h5 className="text-muted fw-bold d-none d-md-block">{text}</h5>
                     <Link to="/me" relative="path" className="link-underline link-underline-opacity-0 d-md-none">
                         <div className="d-flex align-items-center mb-2 w-100 text-muted">
-                            <LeftIcon height="20" width="20" className="me-2 text-tertiary" /> <h5 className=" fw-bold mb-0">{text}</h5>
+                            <LeftIcon height="20" width="20" className="me-2 text-tertiary" /> <h5 className="mb-0">{text}</h5>
                         </div>
                     </Link>
                 </>
