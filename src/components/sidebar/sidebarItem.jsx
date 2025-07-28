7bde795465cf31a8795c5131cb2e5c8e09874032
John <johnsuyang2118@gmail.com>
2025-07-29 05:18:44 +0800

    adjusted sidebarItem component
    sidebaritem now use navLink so that users can click it and have indicator which current page the user is currently on

--- src/components/sidebar/sidebarItem.jsx
+++ src/components/sidebar/sidebarItem.jsx
@@ -1,7 +1,8 @@
-function SidebarItem({ children, isActive, isHeader }) {
-    let className = `list-group-item border-0`;
+import { NavLink } from "react-router";
+
+function SidebarItem({ children, isHeader, to }) {
+    let className = `list-group-item border-0 text-primary`;
     let headerClass = " border-bottom fs-6 fw-bold d-flex align-items-center";
-    let active = " text-tertiary";
 
     if (isHeader) {
         className += headerClass;
@@ -9,11 +10,26 @@ function SidebarItem({ children, isActive, isHeader }) {
         className += " ms-4";
     }
 
-    if (isActive) {
-        className += active;
-    }
-
-    return <li className={className}>{children}</li>;
+    return (
+        <li className={className}>
+            {to ? (
+                <NavLink
+                    to={to}
+                    className={({ isActive }) => {
+                        let classes = "link-underline link-underline-opacity-0 ";
+                        if (isActive) {
+                            classes += "text-tertiary";
+                        }
+                        return classes;
+                    }}
+                >
+                    {children}
+                </NavLink>
+            ) : (
+                <>{children}</>
+            )}
+        </li>
+    );
 }
 
 export default SidebarItem;
