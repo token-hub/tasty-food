49775f80f8ccd67ba27263432a02265ad81e6472
John <johnsuyang2118@gmail.com>
2025-07-28 09:52:10 +0800

    finished Profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,5 +1,32 @@
 function Profile() {
-    return <>Profile</>;
+    return (
+        <form action="" method="POST">
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                    Email
+                </label>
+                <input type="password" required className="form-control" placeholder="johndoe@gmail.com" id="email" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="firstName" className="form-label">
+                    First name
+                </label>
+                <input type="text" required className="form-control" placeholder="John" id="firstName" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="lastName" className="form-label">
+                    Last Name
+                </label>
+                <input type="text" required className="form-control" placeholder="Doe" id="lastName" />
+            </div>
+
+            <div className="d-flex justify-content-end">
+                <button type="submit" className="btn btn-primary text-light">
+                    Submit
+                </button>
+            </div>
+        </form>
+    );
 }
 
 export default Profile;
