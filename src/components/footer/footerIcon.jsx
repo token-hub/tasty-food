a7cbfa15dade7d79b289e0f8ebef1987f7001141
John <johnsuyang2118@gmail.com>
2025-07-29 07:36:44 +0800

    Change the footerIcon into links

--- src/components/footer/footerIcon.jsx
+++ src/components/footer/footerIcon.jsx
@@ -1,9 +1,24 @@
-function FooterIcon({ text, Icon }) {
+import { NavLink } from "react-router";
+
+function FooterIcon({ text, Icon, to }) {
     return (
-        <div className="d-flex justify-content-center flex-column align-items-center">
-            <Icon className="text-primary" height="20" width="20" />
-            <span className="small-text">{text}</span>
-        </div>
+        <NavLink
+            to={to}
+            className={({ isActive }) => {
+                let classes = "link-underline link-underline-opacity-0 d-flex justify-content-center flex-column align-items-center ";
+
+                if (isActive) {
+                    classes += "text-tertiary";
+                } else {
+                    classes += "text-muted";
+                }
+
+                return classes;
+            }}
+        >
+            <Icon height="20" width="20" />
+            <span className="small-text ">{text}</span>
+        </NavLink>
     );
 }
 
--- src/layout/mobileFooter.jsx
+++ src/layout/mobileFooter.jsx
@@ -10,11 +10,11 @@ function MobileFooter() {
         <nav className="navbar fixed-bottom navbar-light border-top">
             <div className="container-fluid">
                 <div className="d-flex justify-content-evenly w-100">
-                    <FooterIcon text="Home" Icon={HomeIcon} />
-                    <FooterIcon text="Recipes" Icon={RecipeIcon} />
-                    <FooterIcon text="Archives" Icon={ArchiveIcon} />
-                    <FooterIcon text="Notifications" Icon={NotificationIcon} />
-                    <FooterIcon text="Me" Icon={UserIcon} />
+                    <FooterIcon to="/" text="Home" Icon={HomeIcon} />
+                    <FooterIcon to="/me/myRecipes" text="Recipes" Icon={RecipeIcon} />
+                    <FooterIcon to="/me/archives" text="Archives" Icon={ArchiveIcon} />
+                    <FooterIcon to="/me/notifications" text="Notifications" Icon={NotificationIcon} />
+                    <FooterIcon to="/me" text="Me" Icon={UserIcon} />
                 </div>
             </div>
         </nav>
