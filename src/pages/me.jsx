52f1b03c687cfd736e58a8b8791fbf535a0c02cd
John <johnsuyang2118@gmail.com>
2025-07-29 06:25:48 +0800

    Create the me component
    It will show the content of this page only on mobile screen and in large screen it will be hidden

---
+++ src/pages/me.jsx
@@ -0,0 +1,14 @@
+function Me() {
+    return (
+        <div className="d-md-none">
+            <h5>
+                Profile <button> -> </button>
+            </h5>
+            <h5>
+                password <button> -> </button>
+            </h5>
+        </div>
+    );
+}
+
+export default Me;
