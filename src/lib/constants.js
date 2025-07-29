15d5592b43b90d386492658e485259787f05ca5e
John <johnsuyang2118@gmail.com>
2025-07-30 04:44:47 +0800

    move the sampleRecipes to the lib constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,1 +1,28 @@
 export const isAuthenticated = false;
+
+export const sampleRecipes = [
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
+    },
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
+    },
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
+    },
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
+    },
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
+    },
+    {
+        name: "Sinigang na baboy",
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
+    }
+];
--- src/pages/myRecipes.jsx
+++ src/pages/myRecipes.jsx
@@ -1,31 +1,6 @@
 import Recipe from "../components/main/recipe";
 import Pagination from "../components/main/pagination";
-const sampleRecipes = [
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
-    },
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
-    },
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
-    },
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
-    },
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
-    },
-    {
-        name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
-    }
-];
+import { sampleRecipes } from "../lib/constants";
 
 function MyRecipes() {
     return (
