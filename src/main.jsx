b570ff5b725ee0bcaec96960993150447c533f90
John <johnsuyang2118@gmail.com>
2025-07-29 10:51:22 +0800

    import the index.css pf pagination to have the rcpagination it's default styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./scss/style.scss";
 import * as bootstrap from "bootstrap";
+import "rc-pagination/assets/index.css";
 
 import { RouterProvider } from "react-router";
 import router from "./router.js";
