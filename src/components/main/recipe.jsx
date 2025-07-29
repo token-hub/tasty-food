6575fb66663d589841d0efb43f5ef4407447896a
John <johnsuyang2118@gmail.com>
2025-07-30 04:47:45 +0800

    Allow Recipe component to accept isArchived props

--- src/components/main/recipe.jsx
+++ src/components/main/recipe.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router";
 
-function Recipe({ imgSource, name, recipeLink }) {
+function Recipe({ imgSource, name, recipeLink, isArchived = false }) {
+    let classes = "card recipe bg-white shadow ";
+    if (isArchived) {
+        classes += "archived";
+    }
+
     return (
-        <div className="card recipe">
+        <div className={classes}>
             <Link to={recipeLink} className="text-muted link-underline link-underline-opacity-0">
                 <div className="recipe_img">
                     <img src={imgSource} className="card-img-top " alt="test image" />
                 </div>
                 <div className="card-body">
-                    <h5 className="card-title mb-0">{name}</h5>
+                    <h5 className="card-title mb-3">{name}</h5>
                 </div>
             </Link>
         </div>
