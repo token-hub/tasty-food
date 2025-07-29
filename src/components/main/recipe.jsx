eca893e928b85df3a54203e0e6b0fd9383382ef4
John <johnsuyang2118@gmail.com>
2025-07-30 03:53:58 +0800

    Make the whole recipe clicking not just the title and accept recipe link

--- src/components/main/recipe.jsx
+++ src/components/main/recipe.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router";
 
-function Recipe({ imgSource, name }) {
+function Recipe({ imgSource, name, recipeLink }) {
     return (
         <div className="card recipe">
-            <div className="recipe_img">
-                <img src={imgSource} className="card-img-top " alt="test image" />
-            </div>
-            <div className="card-body">
-                <Link to="" className="text-muted link-underline link-underline-opacity-0">
+            <Link to={recipeLink} className="text-muted link-underline link-underline-opacity-0">
+                <div className="recipe_img">
+                    <img src={imgSource} className="card-img-top " alt="test image" />
+                </div>
+                <div className="card-body">
                     <h5 className="card-title mb-0">{name}</h5>
-                </Link>
-            </div>
+                </div>
+            </Link>
         </div>
     );
 }
