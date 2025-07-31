c31c4d250eac47761b41b228be1d4f5da43c621a
John <johnsuyang2118@gmail.com>
2025-07-31 10:36:59 +0800

    created a reusable notification component

---
+++ src/components/main/notification.jsx
@@ -0,0 +1,19 @@
+function Notification({ imageSource = "https://placehold.co/300x300/png", imageAlt = "default", title = "", description = "" }) {
+    return (
+        <div className="notification border px-3 py-3 mb-2">
+            <div className="row">
+                <div className="col-3 col-lg-3 col-xl-2">
+                    <div className="d-flex">
+                        <img src={imageSource} alt={imageAlt} className="img-fluid mx-auto" />
+                    </div>
+                </div>
+                <div className="col-9 col-lg-9 col-xl-10 p-0">
+                    <h5 className="fs-7 fw-semibold">{title}</h5>
+                    <p className="text-muted m-0 fs-7">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Notification;
