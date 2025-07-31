76182072512cafb0fc6b70d4c16a2c125fb67a9e
John <johnsuyang2118@gmail.com>
2025-07-31 11:01:11 +0800

    added a date and isNew props to distinguish a bit what's new and what's old notifications

--- src/components/main/notification.jsx
+++ src/components/main/notification.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router";
 
-function Notification({ imageSource = "https://placehold.co/300x300/png", imageAlt = "default", title = "", description = "", link }) {
+function Notification({ imageSource = "https://placehold.co/300x300/png", imageAlt = "default", title = "", description = "", link, isNew = false }) {
+    let classes = "notification border px-3 py-3 mb-2 ";
+    if (isNew) {
+        classes += "bg-white";
+    }
     return (
         <Link to={link} className="link-underline link-underline-opacity-0">
-            <div className="notification border px-3 py-3 mb-2">
+            <div className={classes}>
                 <div className="row">
                     <div className="col-3 col-lg-3 col-xl-2">
                         <div className="d-flex">
@@ -11,8 +15,13 @@ function Notification({ imageSource = "https://placehold.co/300x300/png", imageA
                         </div>
                     </div>
                     <div className="col-9 col-lg-9 col-xl-10 p-0">
-                        <h5 className="fs-7 fw-semibold text-dark">{title}</h5>
-                        <p className="text-muted m-0 fs-7">{description}</p>
+                        <div className="d-flex flex-column h-100">
+                            <div className="flex-grow-1">
+                                <h5 className="fs-7 fw-semibold text-dark">{title}</h5>
+                                <p className="text-muted m-0 fs-7">{description}</p>
+                            </div>
+                            <p className="fs-7 text-muted mb-0">07/28/2025</p>
+                        </div>
                     </div>
                 </div>
             </div>
