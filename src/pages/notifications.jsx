a6cb21408d9fe9134011a233bc8363719f097080
John <johnsuyang2118@gmail.com>
2025-07-31 10:37:25 +0800

    import the resuable component in notifications component

--- src/pages/notifications.jsx
+++ src/pages/notifications.jsx
@@ -1,5 +1,14 @@
+import Notification from "../components/main/notification";
+
 function Notifications() {
-    return <div>Notifications</div>;
+    return (
+        <div className="container">
+            <Notification
+                title="Your recipe has a new rating"
+                description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid quisquam, voluptatem ipsum aperiam provident quasi explicabo dicta repudiandae saepe alias totam repellat perspiciatis autem nulla laboriosam quidem assumenda molestias. Repudiandae?"
+            />
+        </div>
+    );
 }
 
 export default Notifications;
