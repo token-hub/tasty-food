c77dd89c8e423850017808c5efc3a2f171c5f6b6
John <johnsuyang2118@gmail.com>
2025-08-02 06:48:17 +0800

    Update the sampleRecipe to have a desiption

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,26 +3,38 @@ export const isAuthenticated = false;
 export const sampleRecipes = [
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     },
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     },
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     },
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/fried-pork-recipe-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     },
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     },
     {
         name: "Sinigang na baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg"
+        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Vietnamese-Fried-Rice-Recipe-with-Sausage-257x257.jpg",
+        description:
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
     }
 ];
