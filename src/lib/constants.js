aa96249a8b8a0adb8cf8c32c8f285e497962f2c1
John <johnsuyang2118@gmail.com>
2025-08-02 07:38:34 +0800

    added ingrediens and instruction for the first data only in sample recipes array

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,7 +5,34 @@ export const sampleRecipes = [
         name: "Sinigang na baboy",
         imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg",
         description:
-            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice."
+            "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice.",
+        ingredients: [
+            { name: "mung beans", quantity: "14", unit: "ounce", isMain: true },
+            { name: "calabaza squash cubed", quantity: "1", unit: "lb", isMain: false },
+            { name: "cup salted dried shrimp hibe", quantity: "1/3", unit: "cup", isMain: false },
+            { name: "spinach", quantity: "2", unit: "cup", isMain: false },
+            { name: "tomatoes diced", quantity: "2", unit: "cup", isMain: false },
+            { name: "onion chopped", quantity: "1", unit: "pc", isMain: false },
+            { name: "cloves garlic chopped", quantity: "4", unit: "pc", isMain: false },
+            { name: "pork rinds chicharon", quantity: "1", unit: "cup", isMain: false },
+            { name: "Maggi Magic Sarap", quantity: "8", unit: "grams", isMain: false },
+            { name: "fish sauce", quantity: "3", unit: "tablespoon", isMain: false },
+            { name: "ground black pepper", quantity: "1/3", unit: "teaspoon", isMain: false },
+            { name: "water", quantity: "1 1/3", unit: "quarts", isMain: false },
+            { name: "cooking oil", quantity: "3", unit: "tablespoon", isMain: false }
+        ],
+        instructions: [
+            "Soak the mung beans in ½ quart water overnight. 14 ounces mung beans, 1 ½ quarts water",
+            "Heat 3 tablespoons of cooking oil in a deep cooking pot.",
+            "Sauté the garlic until it starts to brown. Add the onion and tomato. Continue sautéing until softened.",
+            "Add the dried shrimp and soaked mung beans. Stir well.",
+            "Pour in the fish sauce and 1 quart of water. Cover and simmer until the mung beans are completely soft and the soup develops a thick consistency. Note: Add more water as needed.",
+            "Add the calabaza squash. Cover and continue cooking until the squash is tender.",
+            "Add the malunggay leaves and spinach. Cook for 1 minute.",
+            "Season with ground black pepper and Maggi Magic Sarap. Stir well.",
+            "Top with crispy chicharon.",
+            "Serve hot with fried pork belly and steamed rice."
+        ]
     },
     {
         name: "Sinigang na baboy",
