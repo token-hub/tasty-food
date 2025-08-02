957f21fd69de28fc235e0008ed86bc699e78c418
John <johnsuyang2118@gmail.com>
2025-08-02 08:48:56 +0800

    import the recipe components

--- src/pages/recipe.jsx
+++ src/pages/recipe.jsx
@@ -1,5 +1,21 @@
+import RecipeHeaders from "../components/main/recipe/recipeHeaders";
+import RecipeIngredients from "../components/main/recipe/recipeIngredients";
+import RecipeInstructions from "../components/main/recipe/recipeInstructions";
+import { sampleRecipes } from "../lib/constants";
+
 function Recipe() {
-    return <div>Recipe</div>;
+    const recipe = sampleRecipes[0];
+
+    return (
+        <div className="container">
+            <RecipeHeaders recipe={recipe} />
+            <hr />
+            <RecipeIngredients recipe={recipe} />
+            <hr />
+            <RecipeInstructions recipe={recipe} />
+            <hr />
+        </div>
+    );
 }
 
 export default Recipe;
