47147bd933078b326e2cdfb80a4a6a5074336adc
John <johnsuyang2118@gmail.com>
2025-08-03 06:41:52 +0800

    fix ingredients section in smaller screen to not let the checkbox take the whole line

--- src/components/main/recipe/recipeIngredients.jsx
+++ src/components/main/recipe/recipeIngredients.jsx
@@ -5,8 +5,8 @@ function RecipeIngredients({ recipe }) {
             <ul class="list-group list-group-flush">
                 {recipe.ingredients.length &&
                     recipe.ingredients.map(({ name, quantity, unit }, index) => (
-                        <li key={index} class="list-group-item py-1 lead">
-                            <input class="form-check-input me-2" type="checkbox" value="" id={index} />
+                        <li key={index} class="list-group-item py-1 lead d-flex d-sm-block">
+                            <input class="form-check-input me-2 mt-2 mt-sm-0 align-self-start" type="checkbox" value="" id={index} />
                             <label class="form-check-label stretched-link" for={index}>
                                 {quantity} {unit} of {name}
                             </label>
