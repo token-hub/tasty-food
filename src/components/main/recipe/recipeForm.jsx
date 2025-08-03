165a69e9e0c4f89dd34ed61cbd9b54a6e372354a
John <johnsuyang2118@gmail.com>
2025-08-03 11:16:49 +0800

    added edit ui
    user can now click the edit icon to edit his/her current recipe rating

--- src/components/main/recipe/recipeForm.jsx
+++ src/components/main/recipe/recipeForm.jsx
@@ -1,32 +1,58 @@
 import { Rating } from "react-simple-star-rating";
+import EditIcon from "../../../assets/icons/editIcon";
+import XIcon from "../../../assets/icons/xIcon";
+import { currentUser } from "../../../lib/constants";
+import { useState } from "react";
+
 function RecipeForm() {
+    // this is only temporary,
+    // the current user own rating will be fetch separately from the recipe rating itself
+    // const [rating, setRating] = useState(currentUser.rating);
+    const [rating, setRating] = useState();
+    const [isEditting, setIsEditting] = useState(false);
+
+    const showSubmitBtn = !rating || isEditting;
+    const isDisabled = !isEditting && rating;
+
     function handleRating() {
         // do something here
+    }
+
+    function handleEdit() {
+        setIsEditting(!isEditting);
     }
 
     return (
         <form className="mt-4">
             <div className="mb-3">
-                <label htmlFor="rating" className="form-label">
-                    Your rating for this recipe:
+                <label htmlFor="rating" className="form-label w-100 d-flex m-0">
+                    <span className="flex-grow-1">Your rating for this recipe:</span>
+                    {rating && (
+                        <button type="button" onClick={handleEdit} className="btn p-1 mb-1 text-primary border-0 ms-auto">
+                            {!isEditting ? <EditIcon /> : <XIcon />}
+                        </button>
+                    )}
                 </label>
-                <Rating onClick={handleRating} />
+                <Rating readonly={isDisabled} onClick={handleRating} initialValue={rating ? rating.rating : 0} />
             </div>
             <div className="form-floating">
                 <textarea
-                    className="form-control bg-light"
+                    className={`form-control ${isDisabled ? "" : "bg-light"}`}
                     placeholder="Leave a comment here ..."
                     id="floatingTextarea2"
                     style={{ height: 100 }}
-                    defaultValue={""}
+                    disabled={isDisabled}
+                    defaultValue={rating ? rating.comment : ""}
                 />
             </div>
 
-            <div className="d-flex">
-                <button type="submit" className="btn btn-primary text-white mt-3 ms-auto">
-                    Submit
-                </button>
-            </div>
+            {showSubmitBtn && (
+                <div className="d-flex">
+                    <button type="submit" className="btn btn-primary text-white mt-3 ms-auto">
+                        Submit
+                    </button>
+                </div>
+            )}
         </form>
     );
 }
