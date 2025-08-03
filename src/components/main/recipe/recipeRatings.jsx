1bdbaf258533468cb75c73941ae44d407b180893
John <johnsuyang2118@gmail.com>
2025-08-03 08:38:01 +0800

    added a default view when there's no ratings yet

--- src/components/main/recipe/recipeRatings.jsx
+++ src/components/main/recipe/recipeRatings.jsx
@@ -6,7 +6,9 @@ function RecipeRatings({ ratings }) {
             {ratings.length ? (
                 ratings.map((rating) => <RecipeRating rating={rating} key={rating.id} />)
             ) : (
-                <div className="">No ratings found ...</div>
+                <div style={{ height: "15rem" }} className="d-flex w-100 justify-content-center align-items-center text-muted">
+                    No ratings found
+                </div>
             )}
         </div>
     );
