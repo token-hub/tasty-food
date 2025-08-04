186f6f08507fd55528fd311d7ba0dcb910defe1f
John <johnsuyang2118@gmail.com>
2025-08-04 10:30:50 +0800

    create the add button for the myRecipe page that will open a modal later

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,14 +1,20 @@
 import { useLocation } from "react-router";
 import MainHeader from "../components/main/mainHeader";
 import { getMainHeaderText } from "../lib/utilities";
+import { PATHS } from "../lib/constants";
 
 function Main({ children }) {
     let { pathname } = useLocation();
     let currentHeader = getMainHeaderText(pathname);
+    let button;
+
+    if (pathname.includes(PATHS.myRecipes.name)) {
+        button = <button className="btn btn-primary text-white mb-3">Add</button>;
+    }
 
     return (
         <main className="py-3 mb-6 px-md-4 bg-light">
-            <MainHeader text={currentHeader} />
+            <MainHeader text={currentHeader} button={button} />
             {children}
         </main>
     );
