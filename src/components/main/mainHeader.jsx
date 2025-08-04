cdec7a23e2f1abab0b46de5fc41f1a6137d85142
John <johnsuyang2118@gmail.com>
2025-08-04 10:30:05 +0800

    allow the mainheader to accept button props for pages that needs a button in the header

--- src/components/main/mainHeader.jsx
+++ src/components/main/mainHeader.jsx
@@ -2,12 +2,16 @@ import { Link } from "react-router";
 
 import LeftIcon from "../../assets/icons/leftIcon";
 
-function MainHeader({ text }) {
+function MainHeader({ text, button }) {
+    let Button = button;
     return (
         <div className={`${text ? "border-bottom  mb-3" : ""}`}>
             {text && (
                 <>
-                    <h5 className="text-muted fw-bold d-none d-md-block">{text}</h5>
+                    <div className="d-flex align-items-center">
+                        <h5 className="text-muted fw-bold d-none d-md-block flex-grow-1">{text}</h5>
+                        {button}
+                    </div>
                     <Link to="/me" relative="path" className="link-underline link-underline-opacity-0 d-md-none">
                         <div className="d-flex align-items-center mb-2 w-100 text-muted">
                             <LeftIcon height="20" width="20" className="me-2 text-tertiary" /> <h5 className="mb-0">{text}</h5>
