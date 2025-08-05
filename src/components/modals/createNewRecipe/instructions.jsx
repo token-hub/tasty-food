fa5063adeeb9f702f62910e4440c695640cfcbc1
John <johnsuyang2118@gmail.com>
2025-08-05 10:30:29 +0800

    Make the add instruction and remove instruction button to work

--- src/components/modals/createNewRecipe/instructions.jsx
+++ src/components/modals/createNewRecipe/instructions.jsx
@@ -1,19 +1,51 @@
-import XIcon from "../../../assets/icons/xIcon";
+import { useState, Fragment } from "react";
+
+import InstructionInput from "./instructionInput";
 
 function Instructions() {
+    const [count, setCount] = useState(0);
+
+    const initialInputs = [
+        {
+            id: count,
+            component: InstructionInput
+        }
+    ];
+
+    const [inputs, setInputs] = useState(initialInputs);
+
+    function addInstruction() {
+        setInputs([
+            ...inputs,
+            {
+                id: count + 1,
+                component: InstructionInput
+            }
+        ]);
+        setCount((count) => count + 1);
+    }
+
+    function removeInstruction(id) {
+        if (count > 0) {
+            setCount((count) => count - 1);
+            const filteredInstructions = inputs.filter((input) => input.id !== id);
+            setInputs(filteredInstructions);
+        }
+    }
+
     return (
         <>
             <span className="d-block mb-3 mt-4">Instructions:</span>
-            <div className="d-flex">
-                <div className="form-floating w-100">
-                    <input type="text" className="form-control mb-3" id="unit" placeholder="1" />
-                    <label htmlFor="unit">{`instruction #1`}</label>
-                </div>
-                <button className="btn text-secondary mb-3">
-                    <XIcon />
-                </button>
-            </div>
-            <button type="button" className="btn border-0">
+            {inputs.map(({ id, component }, index) => {
+                let Component = component;
+                return (
+                    <Fragment key={id}>
+                        <Component id={id} index={index} count={count} removeInstruction={removeInstruction} />
+                    </Fragment>
+                );
+            })}
+
+            <button onClick={addInstruction} type="button" className="btn border-0">
                 <span className="fs-7 text-muted my-3">Add more Instruction ?</span>
             </button>
         </>
