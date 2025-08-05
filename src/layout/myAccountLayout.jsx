d2d5293a20c91d6c80613ed0f94e594c1f9bf565
John <johnsuyang2118@gmail.com>
2025-08-05 20:58:57 +0800

    wrap the layout with recipe and modal provider

--- src/layout/myAccountLayout.jsx
+++ src/layout/myAccountLayout.jsx
@@ -3,31 +3,35 @@ import BaseHeader from "./baseHeader";
 import Sidebar from "../components/sidebar/sidebar";
 import MobileFooter from "./mobileFooter";
 import Main from "./main";
-import CreateRecipeModal from "../components/modals/createRecipeModal";
+import RecipeProvider from "../providers/recipeProvider";
+import ModalProvider from "../providers/modalProvider";
+import CreateRecipeModal from "../components/modals/createNewRecipe/createRecipeModal";
 
 function MyAccountLayout() {
     return (
-        <>
-            <BaseHeader />
-            <div className="container">
-                <div className="row mt-3 ">
-                    <div className="col-md-3 col-lg-2">
-                        <div className="d-none d-md-block">
-                            <Sidebar />
+        <RecipeProvider>
+            <ModalProvider>
+                <BaseHeader />
+                <div className="container">
+                    <div className="row mt-3 ">
+                        <div className="col-md-3 col-lg-2">
+                            <div className="d-none d-md-block">
+                                <Sidebar />
+                            </div>
+                        </div>
+                        <div className="col-md-9 col-lg-10">
+                            <Main>
+                                <Outlet />
+                            </Main>
                         </div>
                     </div>
-                    <div className="col-md-9 col-lg-10">
-                        <Main>
-                            <Outlet />
-                        </Main>
-                    </div>
                 </div>
-            </div>
-            <div className="d-md-none">
-                <MobileFooter />
-            </div>
-            <CreateRecipeModal />
-        </>
+                <div className="d-md-none">
+                    <MobileFooter />
+                </div>
+                <CreateRecipeModal />
+            </ModalProvider>
+        </RecipeProvider>
     );
 }
 
