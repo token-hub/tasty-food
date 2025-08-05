7d81889270eeae329cf444d3460d9d69d569c115
John <johnsuyang2118@gmail.com>
2025-08-05 08:21:32 +0800

    Attach ref that will open the file explorer to selecte image

--- src/components/modals/createNewRecipe/uploadImage.jsx
+++ src/components/modals/createNewRecipe/uploadImage.jsx
@@ -1,21 +1,28 @@
+import { useRef } from "react";
 import UpArrowIcon from "../../../assets/icons/UpArrowIcon";
 
 function CreateModalUploadImage({ imageSrc = "https://placehold.co/200x200?font=roboto&text=Recipe", nameOfRecipe = "image of new recipe" }) {
+    const uploadRef = useRef();
+
+    function handleUpload() {
+        uploadRef.current.click();
+    }
+
     return (
         <>
             <div className="mb-3 d-flex justify-content-center">
                 <div className="position-relative d-inline-flex ">
                     <img height="200" width="200" src={imageSrc} alt={nameOfRecipe} className=" rounded-circle" />
-                    <div className="position-absolute bottom-10 end-10">
+                    <div className="position-absolute bottom-10 end-10" role="button" onClick={handleUpload}>
                         <UpArrowIcon height="28" width="28" className="text-muted" />
                     </div>
                 </div>
             </div>
             <div className="invisible" style={{ height: 0 }}>
-                <label htmlFor="formFile" className="form-label">
+                <label htmlFor="imageFile" className="form-label">
                     Upload Image of the recipe
                 </label>
-                <input className="form-control" type="file" id="formFile" />
+                <input ref={uploadRef} className="form-control" type="file" id="imageFile" accept="image/png, image/gif, image/jpeg" />
             </div>
         </>
     );
