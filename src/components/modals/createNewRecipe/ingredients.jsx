8c7dcf850ddb7c34c95094dd3c6fd8b196fccacd
John <johnsuyang2118@gmail.com>
2025-08-05 10:13:23 +0800

    Make the add ingredient and remove ingredient buttons to work

--- src/components/modals/createNewRecipe/ingredients.jsx
+++ src/components/modals/createNewRecipe/ingredients.jsx
@@ -1,28 +1,50 @@
-import XIcon from "../../../assets/icons/xIcon";
+import { useState, Fragment } from "react";
+import IngredientsInputs from "./ingredientsInputs";
 
 function Ingredients() {
+    const [count, setCount] = useState(0);
+
+    const initialInputs = [
+        {
+            id: count,
+            component: IngredientsInputs
+        }
+    ];
+
+    const [inputs, setInputs] = useState(initialInputs);
+
+    function addIngredient() {
+        setInputs([
+            ...inputs,
+            {
+                id: count + 1,
+                component: IngredientsInputs
+            }
+        ]);
+        setCount((count) => count + 1);
+    }
+
+    function removeIngredient(id) {
+        if (count > 0) {
+            setCount((count) => count - 1);
+            const filteredIngredients = inputs.filter((input) => input.id !== id);
+            setInputs(filteredIngredients);
+        }
+    }
+
     return (
         <>
             <span className="d-block mb-3 mt-4">Ingredients:</span>
 
-            <div className="d-flex w-100 align-items-center h-100">
-                <div className="form-floating w-100">
-                    <input type="number" min="0" className="form-control mb-3" id="quantity" placeholder="1" />
-                    <label htmlFor="quantity">Quantity</label>
-                </div>
-                <div className="form-floating w-100">
-                    <input type="text" className="form-control mb-3" id="unit" placeholder="1" />
-                    <label htmlFor="unit">Unit</label>
-                </div>
-                <div className="form-floating w-100">
-                    <input type="text" className="form-control mb-3" id="ingredient" placeholder="1" />
-                    <label htmlFor="ingredient">Ingredient</label>
-                </div>
-                <button className="btn text-secondary mb-3">
-                    <XIcon />
-                </button>
-            </div>
-            <button type="button" className="btn border-0">
+            {inputs.map(({ id, component }) => {
+                let Component = component;
+                return (
+                    <Fragment key={id}>
+                        <Component id={id} count={count} removeIngredient={removeIngredient} />
+                    </Fragment>
+                );
+            })}
+            <button onClick={addIngredient} type="button" className="btn border-0">
                 <span className="fs-7 text-muted my-3">Add more Ingredients ?</span>
             </button>
         </>
