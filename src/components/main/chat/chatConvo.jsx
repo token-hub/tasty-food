ecf4ca499302925fd6492dd53be248e0e4c01be4
John <johnsuyang2118@gmail.com>
2025-08-07 00:11:05 +0800

    created a reusable chat convo component

--- src/components/main/chat/chatConvo.jsx
+++ src/components/main/chat/chatConvo.jsx
@@ -1,5 +1,12 @@
 import UserIcon from "../../../assets/icons/userIcon";
-function ChatConvo() {
+import { getDayAndMonthFromDate, trimTextAddEllipsis, capitalizeFirstLetter } from "../../../lib/utilities";
+
+function ChatConvo({ name, date, text, convoCount = 0 }) {
+    const convoDate = getDayAndMonthFromDate(date);
+    let trimName = trimTextAddEllipsis(name, 8);
+    let trimText = trimTextAddEllipsis(text, 13);
+    trimText = capitalizeFirstLetter(trimText);
+
     return (
         <div className="d-flex align-items-center p-2" role="button">
             <div className="border rounded-circle">
@@ -7,12 +14,19 @@ function ChatConvo() {
             </div>
             <div className="fs-7 px-2 py-1">
                 <p className="m-0 d-flex align-items-center">
-                    <span className="fw-bold me-auto">John Doe</span>
-                    <span className="small">26/01</span>
+                    <span className="fw-bold me-auto text-capitalize">{trimName}</span>
+                    <span className="small">{convoDate}</span>
                 </p>
                 <p className="m-0">
-                    <span className="text-muted small me-2">Very importa...</span>
-                    <span className="convo-count small bg-secondary text-white  rounded-circle">10</span>
+                    <span className="text-muted small me-2">{trimText}</span>
+                    {convoCount > 0 && (
+                        <span
+                            className="convo-count small bg-secondary text-white  rounded-circle d-inline-block text-center"
+                            style={{ width: "15px" }}
+                        >
+                            {convoCount}
+                        </span>
+                    )}
                 </p>
             </div>
         </div>
