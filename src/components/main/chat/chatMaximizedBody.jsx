305e405478229b43aa93e9bd01a89e726282d675
John <johnsuyang2118@gmail.com>
2025-08-07 00:14:06 +0800

    import the convo date and message inside chat body

---
+++ src/components/main/chat/chatMaximizedBody.jsx
@@ -0,0 +1,26 @@
+import ConvoDate from "./convoDate";
+import ConvoMessage from "./convoMessage";
+import ChatArea from "./chatArea";
+
+function ChatMaximizedBody({ convoWith }) {
+    return (
+        <div className="mt-2 h-100 position-relative">
+            <p className="fs-7 m-0 mb-1 fw-bold px-3">{convoWith}</p>
+            <hr className="m-0" />
+            <div className="h-100 p-3 overflow-auto">
+                <div className="d-flex flex-column mb-6">
+                    <ConvoDate date={"2020/07/26"} />
+                    <ConvoMessage
+                        date={new Date()}
+                        message="Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, dolor! Est fugiat incidunt enim dolore quisquam
+                            explicabo recusandae delectus consequatur voluptatem et laudantium reprehenderit corrupti aut sapiente deserunt, quo a."
+                    />
+                    <ConvoMessage date={new Date()} isUser={true} message="Hello Author!" />
+                </div>
+            </div>
+            <ChatArea />
+        </div>
+    );
+}
+
+export default ChatMaximizedBody;
