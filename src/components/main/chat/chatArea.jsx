15d9c7f8720573e27593606a42d4e32e0a066391
John <johnsuyang2118@gmail.com>
2025-08-07 00:10:45 +0800

    created the chat area own component

---
+++ src/components/main/chat/chatArea.jsx
@@ -0,0 +1,19 @@
+import SendIcon from "../../../assets/icons/sendIcon";
+
+function ChatArea() {
+    return (
+        <div className="position-absolute bottom-0 w-100 chat-area-container">
+            <textarea
+                className="w-100 border-0 border-top ps-2 py-2 pe-5 small chat-area lh-sm"
+                placeholder="Type your message here..."
+                name=""
+                id=""
+            ></textarea>
+            <div className="position-absolute bottom-0 end-0" role="button">
+                <SendIcon className="send-icon text-primary" height="20" width="20" />
+            </div>
+        </div>
+    );
+}
+
+export default ChatArea;
