c519d6ca5b99868f56a66930f1af05993b85d0ac
John <johnsuyang2118@gmail.com>
2025-08-07 00:10:24 +0800

    add chat icon for chat send button

---
+++ src/assets/icons/sendIcon.jsx
@@ -0,0 +1,16 @@
+function SendIcon({ className, height = "16", width = "16" }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            fill="currentColor"
+            className={`bi bi-send ${className}`}
+            viewBox="0 0 16 16"
+        >
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+        </svg>
+    );
+}
+
+export default SendIcon;
