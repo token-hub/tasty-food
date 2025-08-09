2ada5c4961a0b78d32b42134f54ba2482bc91236
John <johnsuyang2118@gmail.com>
2025-08-09 10:29:54 +0800

    import and wrap the layout with the slide provider

--- src/layout/myAccountLayout.jsx
+++ src/layout/myAccountLayout.jsx
@@ -1,39 +1,55 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+
+// Providers
+import RecipeProvider from "../providers/recipeProvider";
+import ModalProvider from "../providers/modalProvider";
+import SlideProvider from "../providers/slideProvider";
+
+// Components
+import CreateRecipeModal from "../components/modals/createNewRecipe/createRecipeModal";
+import Chat from "../components/main/chat/chat";
+import MobileSlide from "../components/slides/mobileSlide";
 import BaseHeader from "./baseHeader";
 import Sidebar from "../components/sidebar/sidebar";
 import MobileFooter from "./mobileFooter";
 import Main from "./main";
-import RecipeProvider from "../providers/recipeProvider";
-import ModalProvider from "../providers/modalProvider";
-import CreateRecipeModal from "../components/modals/createNewRecipe/createRecipeModal";
-import Chat from "../components/main/chat/chat";
 
 function MyAccountLayout() {
+    const [isSlideOpen, setIsSlideOpen] = useState(false);
+
     return (
-        <RecipeProvider>
-            <ModalProvider>
-                <BaseHeader />
-                <div className="container">
-                    <div className="row mt-3 ">
-                        <div className="col-md-3 col-lg-2">
-                            <div className="d-none d-md-block">
-                                <Sidebar />
+        <div className="position-relative">
+            <RecipeProvider>
+                <ModalProvider>
+                    <SlideProvider>
+                        <MobileSlide isSlideOpen={isSlideOpen} setIsSlideOpen={setIsSlideOpen} />
+                        <BaseHeader />
+                        <div className="container">
+                            <div className="row mt-3 ">
+                                <div className="col-md-3 col-lg-2">
+                                    <div className="d-none d-md-block">
+                                        <Sidebar />
+                                    </div>
+                                </div>
+                                <div className="col-md-9 col-lg-10">
+                                    <Main>
+                                        <Outlet />
+                                    </Main>
+                                </div>
                             </div>
+
+                            <Chat />
                         </div>
-                        <div className="col-md-9 col-lg-10">
-                            <Main>
-                                <Outlet />
-                            </Main>
+                        <div className="d-md-none ">
+                            <MobileFooter />
                         </div>
-                    </div>
-                    <Chat />
-                </div>
-                <div className="d-md-none">
-                    <MobileFooter />
-                </div>
-                <CreateRecipeModal />
-            </ModalProvider>
-        </RecipeProvider>
+
+                        <CreateRecipeModal />
+                    </SlideProvider>
+                </ModalProvider>
+            </RecipeProvider>
+        </div>
     );
 }
 
