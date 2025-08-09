2f528f7fab94e63f3c3ceec9cc99f47a2c80009c
John <johnsuyang2118@gmail.com>
2025-08-09 10:24:46 +0800

    add initial slide component

---
+++ src/components/slides/mobileSlide.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+function MobileSlide({ children, isSlideOpen, setIsSlideOpen }) {
+    useEffect(() => {
+        const slide = document.querySelector(".slide");
+        const html = document.querySelector("html");
+
+        if (isSlideOpen) {
+            slide.classList.remove("hidden-from-left-to-right");
+            slide.classList.add("show-from-right-to-left");
+            html.classList.add("overflow-y-hidden");
+        } else {
+            html.classList.remove("overflow-y-hidden");
+            slide.classList.add("hidden-from-left-to-right");
+            slide.classList.remove("show-from-right-to-left");
+        }
+    }, [isSlideOpen]);
+
+    return (
+        <div className="slide hidden-from-left-to-right d-md-none">
+            <span onClick={() => setIsSlideOpen(false)}>{`<--`}</span>
+            {children}
+        </div>
+    );
+}
+
+export default MobileSlide;
