87bee9d47c6b23d1fd0d46f3fed6e99be83c05d4
John <johnsuyang2118@gmail.com>
2025-08-09 10:34:14 +0800

    add the slide state to the slide provider

--- src/providers/slideProvider.jsx
+++ src/providers/slideProvider.jsx
@@ -1,13 +1,26 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-const SlideContext = createContext();
+const SlideContext = createContext({
+    isSlideOpen: false,
+    handleSlide: () => {}
+});
 
 export function useSlideContext() {
     return useContext(SlideContext);
 }
 
 function SlideProvider({ children }) {
-    const values = {};
+    const [isSlideOpen, setIsSlideOpen] = useState(false);
+
+    function handleSlide(isOpen) {
+        setIsSlideOpen(isOpen);
+    }
+
+    const values = {
+        isSlideOpen,
+        handleSlide
+    };
+
     return <SlideContext value={values}>{children}</SlideContext>;
 }
 
