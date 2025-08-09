0eeb0119c70743504c97a7abc939a2e9d0e49a28
John <johnsuyang2118@gmail.com>
2025-08-09 12:11:03 +0800

    update the usage of slide from boolean into object

--- src/components/slides/mobileSlide.jsx
+++ src/components/slides/mobileSlide.jsx
@@ -2,33 +2,34 @@ import { useEffect } from "react";
 import { useSlideContext } from "../../providers/slideProvider";
 import LeftIcon from "../../assets/icons/leftIcon";
 import ChatDotsIcon from "../../assets/icons/ChatDotsIcon";
-import Profile from "../../pages/profile";
+import Profile from "../../pages/password";
 
 function MobileSlide({ children }) {
-    const { isSlideOpen, handleSlide } = useSlideContext();
+    const { slide, handleSlide } = useSlideContext();
+    const isSlideOpen = slide.open;
 
     useEffect(() => {
-        const slide = document.querySelector(".slide");
+        const slideElem = document.querySelector(".slide");
         const html = document.querySelector("html");
 
         if (isSlideOpen) {
-            slide.classList.remove("hidden-from-left-to-right");
-            slide.classList.add("show-from-right-to-left");
+            slideElem.classList.remove("hidden-from-left-to-right");
+            slideElem.classList.add("show-from-right-to-left");
             html.classList.add("overflow-y-hidden");
         } else {
             html.classList.remove("overflow-y-hidden");
-            slide.classList.add("hidden-from-left-to-right");
-            slide.classList.remove("show-from-right-to-left");
+            slideElem.classList.add("hidden-from-left-to-right");
+            slideElem.classList.remove("show-from-right-to-left");
         }
     }, [isSlideOpen]);
 
     return (
         <div className="slide hidden-from-left-to-right d-md-none">
             <div className="d-flex align-items-center justify-content-between p-3 shadow-sm">
-                <button className="btn p-0" onClick={() => handleSlide(false)}>
+                <button className="btn p-0" onClick={() => handleSlide({ open: false })}>
                     <LeftIcon className="text-secondary" height="26" width="26" />
                 </button>
-                <h5 className="m-0">Profile</h5>
+                <h5 className="m-0 text-capitalize">{slide.header}</h5>
                 <ChatDotsIcon className="text-secondary" height="22" width="22" />
             </div>
             <Profile />
