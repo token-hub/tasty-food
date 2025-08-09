d0b5b62b6453dac19ca6f6032ca9f5822c0ce1db
John <johnsuyang2118@gmail.com>
2025-08-09 11:32:40 +0800

    add initial mobile slide header

--- src/components/slides/mobileSlide.jsx
+++ src/components/slides/mobileSlide.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useSlideContext } from "../../providers/slideProvider";
+import LeftIcon from "../../assets/icons/leftIcon";
+import ChatDotsIcon from "../../assets/icons/ChatDotsIcon";
+import Profile from "../../pages/profile";
 
 function MobileSlide({ children }) {
     const { isSlideOpen, handleSlide } = useSlideContext();
@@ -21,7 +24,14 @@ function MobileSlide({ children }) {
 
     return (
         <div className="slide hidden-from-left-to-right d-md-none">
-            <span onClick={() => handleSlide(false)}>{`<--`}</span>
+            <div className="d-flex align-items-center justify-content-between p-3 shadow-sm">
+                <button className="btn p-0" onClick={() => handleSlide(false)}>
+                    <LeftIcon className="text-secondary" height="26" width="26" />
+                </button>
+                <h5 className="m-0">Profile</h5>
+                <ChatDotsIcon className="text-secondary" height="22" width="22" />
+            </div>
+            <Profile />
             {children}
         </div>
     );
