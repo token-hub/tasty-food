80a1a718d2bdbdfd84d8c659243939b8be32719a
John <johnsuyang2118@gmail.com>
2025-08-09 12:12:11 +0800

    let the link to open the mobile slide when clicking a submenu

--- src/pages/me.jsx
+++ src/pages/me.jsx
@@ -1,10 +1,14 @@
-import SubmenuLink from "../components/main/submenuLink";
+import Submenu from "../components/main/submenu";
+import { useSlideContext } from "../providers/slideProvider";
+import { SUBMENU_HEADERS } from "../lib/constants";
 
 function Me() {
+    const { handleSlide } = useSlideContext();
+
     return (
-        <div className="d-md-none">
-            <SubmenuLink to="profile" text="profile" />
-            <SubmenuLink to="password" text="password" />
+        <div className="d-md-none px-3">
+            <Submenu clickHandler={() => handleSlide({ open: true, header: SUBMENU_HEADERS[0] })} text={SUBMENU_HEADERS[0]} />
+            <Submenu clickHandler={() => handleSlide({ open: true, header: SUBMENU_HEADERS[1] })} text={SUBMENU_HEADERS[1]} />
         </div>
     );
 }
