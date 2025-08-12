6780f7e3b08bcfb123106e5c1d2fcae5cb9780bc
John <johnsuyang2118@gmail.com>
2025-08-12 22:00:54 +0800

    adjust chatConvo for mobile view

--- src/components/main/chat/chatConvo.jsx
+++ src/components/main/chat/chatConvo.jsx
@@ -1,18 +1,20 @@
 import UserIcon from "../../../assets/icons/userIcon";
 import { getDayAndMonthFromDate, trimTextAddEllipsis, capitalizeFirstLetter } from "../../../lib/utilities";
 
-function ChatConvo({ name, date, text, convoCount = 0 }) {
+function ChatConvo({ name, date, text, convoCount = 0, mobileView = true }) {
+    let trimNameLength = mobileView ? 27 : 8;
+    let trimTextLength = mobileView ? 35 : 13;
     const convoDate = getDayAndMonthFromDate(date);
-    let trimName = trimTextAddEllipsis(name, 8);
-    let trimText = trimTextAddEllipsis(text, 13);
+    let trimName = trimTextAddEllipsis(name, trimNameLength);
+    let trimText = trimTextAddEllipsis(text, trimTextLength);
     trimText = capitalizeFirstLetter(trimText);
 
     return (
-        <div className="d-flex align-items-center p-2" role="button">
-            <div className="border rounded-circle">
+        <div className="d-flex align-items-center p-2 " role="button">
+            <div className={`border rounded-circle ${mobileView ? "me-2" : ""}`}>
                 <UserIcon height="28" width="28" />
             </div>
-            <div className="fs-7 px-2 py-1">
+            <div className="fs-7 px-2 py-1 w-100 w-sm-0 border-bottom">
                 <p className="m-0 d-flex align-items-center">
                     <span className="fw-bold me-auto text-capitalize">{trimName}</span>
                     <span className="small">{convoDate}</span>
