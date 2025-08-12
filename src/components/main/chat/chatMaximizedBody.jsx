b324b7a3e4c88d476ecfc2be00a6d0007bb2537d
John <johnsuyang2118@gmail.com>
2025-08-12 22:38:39 +0800

    adjust chatMaximizedBody to be able to use too in mobile view

--- src/components/main/chat/chatMaximizedBody.jsx
+++ src/components/main/chat/chatMaximizedBody.jsx
@@ -2,11 +2,16 @@ import ConvoDate from "./convoDate";
 import ConvoMessage from "./convoMessage";
 import ChatArea from "./chatArea";
 
-function ChatMaximizedBody({ convoWith }) {
+function ChatMaximizedBody({ convoWith, mobileView = false }) {
     return (
-        <div className="mt-2 h-100 position-relative">
-            <p className="fs-7 m-0 mb-1 fw-bold px-3">{convoWith}</p>
-            <hr className="m-0" />
+        <div className={`mt-2 ${mobileView ? "h-90" : "h-100"} position-relative`}>
+            {!mobileView && (
+                <>
+                    <p className="fs-7 m-0 mb-1 fw-bold px-3">{convoWith}</p>
+                    <hr className="m-0" />
+                </>
+            )}
+
             <div className="h-100 p-3 overflow-auto">
                 <div className="d-flex flex-column mb-6">
                     <ConvoDate date={"2020/07/26"} />
