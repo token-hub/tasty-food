ac9ad549d398fe5bd8eed414ff7a124632321f26
John <johnsuyang2118@gmail.com>
2025-08-13 19:23:52 +0800

    add the component that will show when profile and password buttons were clicked

--- src/pages/me.jsx
+++ src/pages/me.jsx
@@ -1,14 +1,16 @@
 import Submenu from "../components/main/submenu";
 import { useSlideContext } from "../providers/slideProvider";
 import { SUBMENU_HEADERS } from "../lib/constants";
+import Profile from "../pages/profile";
+import Password from "../pages/password";
 
 function Me() {
     const { openSlide } = useSlideContext();
 
     return (
         <div className="d-md-none px-3">
-            <Submenu clickHandler={() => openSlide({ open: true, header: SUBMENU_HEADERS[0] })} text={SUBMENU_HEADERS[0]} />
-            <Submenu clickHandler={() => openSlide({ open: true, header: SUBMENU_HEADERS[1] })} text={SUBMENU_HEADERS[1]} />
+            <Submenu clickHandler={() => openSlide({ open: true, header: SUBMENU_HEADERS[0], component: <Profile /> })} text={SUBMENU_HEADERS[0]} />
+            <Submenu clickHandler={() => openSlide({ open: true, header: SUBMENU_HEADERS[1], component: <Password /> })} text={SUBMENU_HEADERS[1]} />
         </div>
     );
 }
