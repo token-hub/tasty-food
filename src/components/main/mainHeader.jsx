04b4cb34ef0eb0054591bf6377c01991c71e7b02
John <johnsuyang2118@gmail.com>
2025-08-16 22:22:50 +0800

    turn the header into just a regular text and add paddings

--- src/components/main/mainHeader.jsx
+++ src/components/main/mainHeader.jsx
@@ -1,22 +1,12 @@
-import { Link } from "react-router";
-
-import LeftIcon from "../../assets/icons/leftIcon";
-
 function MainHeader({ text, button }) {
-    let Button = button;
     return (
         <div className={`${text ? "border-bottom  mb-3" : ""}`}>
             {text && (
                 <>
-                    <div className="d-flex align-items-center">
-                        <h5 className="text-muted fw-bold d-none d-md-block flex-grow-1">{text}</h5>
+                    <div className="d-flex align-items-center px-3">
+                        <h5 className="text-muted fw-bold flex-grow-1">{text}</h5>
                         {button}
                     </div>
-                    <Link to="/me" relative="path" className="link-underline link-underline-opacity-0 d-md-none">
-                        <div className="d-flex align-items-center mb-2 w-100 text-muted">
-                            <LeftIcon height="20" width="20" className="me-2 text-tertiary" /> <h5 className="mb-0">{text}</h5>
-                        </div>
-                    </Link>
                 </>
             )}
         </div>
