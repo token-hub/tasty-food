eacd06f5e19e57b525f52039b8d53c4e888b9677
John <johnsuyang2118@gmail.com>
2025-08-16 23:28:35 +0800

    add progress state and handleNext func

--- src/components/modals/createNewRecipe/createRecipeModal.jsx
+++ src/components/modals/createNewRecipe/createRecipeModal.jsx
@@ -1,5 +1,4 @@
-import XIcon from "../../../assets/icons/xIcon";
-
+import { useState } from "react";
 import UploadImage from "./uploadImage";
 import UploadVideo from "./uploadVideo";
 import Ingredients from "./ingredients";
@@ -13,12 +12,18 @@ import { useModalContext } from "../../../providers/modalProvider";
 import { MODAL_MODES } from "../../../lib/constants";
 
 function CreateRecipeModal() {
+    const progressStep = 25; // for 4 parts of the modal that will sum to 100
+    const [progress, setProgress] = useState(progressStep);
     const { recipe } = useRecipeContext();
     const {
         modal: { name, mode, show },
         reset
     } = useModalContext();
     let dataToUse = mode === MODAL_MODES[0] ? {} : recipe;
+
+    function handleNext() {
+        setProgress((prev) => prev + progressStep);
+    }
 
     return (
         <>
@@ -34,7 +39,7 @@ function CreateRecipeModal() {
                         <div className="modal-body">
                             {show && (
                                 <form className="form-floating">
-                                    <ProgressBar now={25} />
+                                    <ProgressBar now={progress} />
                                     <UploadImage recipe={dataToUse} />
                                     <NameAndDiscription recipe={dataToUse} />
                                     <Timers recipe={dataToUse} />
@@ -49,7 +54,7 @@ function CreateRecipeModal() {
                             <button type="button" onClick={reset} disabled className="btn btn-gray-dark text-white" data-bs-dismiss="modal">
                                 Previous
                             </button>
-                            <button type="button" className="btn btn-primary text-white">
+                            <button type="button" className="btn btn-primary text-white" onClick={handleNext}>
                                 Next
                             </button>
                         </div>
