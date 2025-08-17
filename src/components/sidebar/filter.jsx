89b47a8e81e41cf1386c0f919b8f430ab3e52675
John <johnsuyang2118@gmail.com>
2025-08-17 12:54:13 +0800

    create filter component

---
+++ src/components/sidebar/filter.jsx
@@ -0,0 +1,24 @@
+import FilterIcon from "../../assets/icons/filterIcon";
+import { RECIPE_CATEGORIES } from "../../lib/constants";
+
+function Filter() {
+    const categories = Object.values(RECIPE_CATEGORIES);
+    return (
+        <>
+            <div className="d-flex align-items-center py-2">
+                <FilterIcon />
+                <h6 className="m-0 ms-1 fw-semibold">Search Filter</h6>
+            </div>
+            {categories.map((category) => (
+                <div className="form-check my-1" key={category}>
+                    <input className="form-check-input border-gray-dark" type="checkbox" defaultValue="" id="checkDefault" />
+                    <label className="form-check-label fs-7 text-capitalize" htmlFor="checkDefault">
+                        {category}
+                    </label>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default Filter;
