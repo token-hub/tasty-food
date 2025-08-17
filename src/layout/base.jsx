aa0bb6017a81ba74b56271dfa9717bd4b188f8d1
John <johnsuyang2118@gmail.com>
2025-08-17 12:54:38 +0800

    import filter component as sidebar

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -4,6 +4,7 @@ import Main from "./main";
 import Chat from "../components/main/chat/chat";
 import { isAuthenticated } from "../lib/constants";
 import SlideProvider from "../providers/slideProvider";
+import Filter from "../components/sidebar/filter";
 function Base() {
     return (
         <SlideProvider>
@@ -11,7 +12,7 @@ function Base() {
             <div className="container">
                 <div className="d-none d-md-block">
                     <div className="row mt-3 ">
-                        <div className="col-md-3 col-lg-2">{/* <Sidebar /> */}</div>
+                        <div className="col-md-3 col-lg-2">{<Filter />}</div>
                         <div className="col-md-9 col-lg-10">
                             <Main>
                                 <Outlet />
