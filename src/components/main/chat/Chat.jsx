5a53b5c88202954c176f1c9abdce9b02412fe0c6
John <johnsuyang2118@gmail.com>
2025-08-18 18:35:45 +0800

    fix open chat function name

--- src/components/main/chat/Chat.jsx
+++ src/components/main/chat/Chat.jsx
@@ -3,13 +3,13 @@ import ChatMaximized from "./chatMaximized";
 import { useChatContext } from "../../../providers/chatProvider";
 
 function Chat() {
-    const { openChat, handleMaxChat, handleCloseChat } = useChatContext();
+    const { openChat, handleOpenChat, handleCloseChat } = useChatContext();
     let chatCount = 99;
 
     return (
         <>
             {/* minimized */}
-            <ChatMinimized isMaxChatOpen={openChat} chatCount={chatCount} onClick={handleMaxChat} />
+            <ChatMinimized isMaxChatOpen={openChat} chatCount={chatCount} onClick={handleOpenChat} />
 
             {/* maximized */}
             <ChatMaximized isOpen={openChat} onClick={handleCloseChat} chatCount={chatCount} />
