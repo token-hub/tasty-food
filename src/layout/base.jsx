822c4aaeaa51b600deaf8df8ba97e9db2f9ec180
John <johnsuyang2118@gmail.com>
2025-08-18 18:34:28 +0800

    import the chat provider in the layout

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -6,25 +6,28 @@ import { isAuthenticated } from "../lib/constants";
 import SlideProvider from "../providers/slideProvider";
 import Filter from "../components/sidebar/filter";
 import RecipeProvider from "../providers/recipeProvider";
+import ChatProvider from "../providers/chatProvider";
 
 function Base() {
     return (
         <SlideProvider>
             <RecipeProvider>
-                <BaseHeader />
-                <div className="container">
-                    <div className="d-none d-md-block">
-                        <div className="row mt-3 ">
-                            <div className="col-md-3 col-lg-2">{<Filter />}</div>
-                            <div className="col-md-9 col-lg-10">
-                                <Main>
-                                    <Outlet />
-                                </Main>
+                <ChatProvider>
+                    <BaseHeader />
+                    <div className="container">
+                        <div className="d-none d-md-block">
+                            <div className="row mt-3 ">
+                                <div className="col-md-3 col-lg-2">{<Filter />}</div>
+                                <div className="col-md-9 col-lg-10">
+                                    <Main>
+                                        <Outlet />
+                                    </Main>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                {isAuthenticated && <Chat />}
+                    {isAuthenticated && <Chat />}
+                </ChatProvider>
             </RecipeProvider>
         </SlideProvider>
     );
