6689160a52179bffc2270711e84d65fcdf3676b4
John <johnsuyang2118@gmail.com>
2025-08-18 12:23:35 +0800

    adjust the recipes that will show dipending on if there;s a selected author

--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -1,13 +1,18 @@
 import Recipe from "../components/main/recipe/recipeClickable";
 import Pagination from "../components/main/pagination";
 import { sampleRecipes } from "../lib/constants";
+import { useParams } from "react-router";
 
 function Recipes() {
+    const params = useParams();
+    const author = params?.author;
+    const recipesOfTheAuthor = sampleRecipes; // temporary
+    const recipes = author ? recipesOfTheAuthor : sampleRecipes;
     return (
         <div className="container">
             <div className="row">
-                {sampleRecipes &&
-                    sampleRecipes.map((recipe) => {
+                {recipes &&
+                    recipes.map((recipe) => {
                         return (
                             <div key={recipe.name} className=" col-md-6 col-xl-4 mb-3">
                                 <Recipe recipe={recipe} />
