49bd09ab39a79069a407dbba791b9da93166aeea
John <johnsuyang2118@gmail.com>
2025-08-18 13:50:11 +0800

    add the recipe provider in the base layout

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -5,23 +5,27 @@ import Chat from "../components/main/chat/chat";
 import { isAuthenticated } from "../lib/constants";
 import SlideProvider from "../providers/slideProvider";
 import Filter from "../components/sidebar/filter";
+import RecipeProvider from "../providers/recipeProvider";
+
 function Base() {
     return (
         <SlideProvider>
-            <BaseHeader />
-            <div className="container">
-                <div className="d-none d-md-block">
-                    <div className="row mt-3 ">
-                        <div className="col-md-3 col-lg-2">{<Filter />}</div>
-                        <div className="col-md-9 col-lg-10">
-                            <Main>
-                                <Outlet />
-                            </Main>
+            <RecipeProvider>
+                <BaseHeader />
+                <div className="container">
+                    <div className="d-none d-md-block">
+                        <div className="row mt-3 ">
+                            <div className="col-md-3 col-lg-2">{<Filter />}</div>
+                            <div className="col-md-9 col-lg-10">
+                                <Main>
+                                    <Outlet />
+                                </Main>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {isAuthenticated && <Chat />}
+                {isAuthenticated && <Chat />}
+            </RecipeProvider>
         </SlideProvider>
     );
 }
