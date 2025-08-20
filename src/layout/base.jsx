ff3068b85dc096ac7c530e5a293404429e58d86c
John <johnsuyang2118@gmail.com>
2025-08-20 21:53:23 +0800

    make the base component layout only about providers and modal
    so I don't have to repeat calling the providers foreach layout

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -1,44 +1,20 @@
-import { Outlet, useParams } from "react-router";
-import BaseHeader from "./baseHeader";
-import Main from "./main";
-import Chat from "../components/main/chat/chat";
-import { isAuthenticated } from "../lib/constants";
+import { Outlet } from "react-router";
 import SlideProvider from "../providers/slideProvider";
-import Filter from "../components/sidebar/filter";
-import FilterMobile from "../components/sidebar/filterMobile";
 import RecipeProvider from "../providers/recipeProvider";
 import ChatProvider from "../providers/chatProvider";
 
 function Base() {
-    const params = useParams();
-    const leftSideClasses = params?.recipe ? "col-md-1" : "col-md-3 col-lg-2";
-    const rightSideClasses = params?.recipe ? "col-md-10" : "col-md-9 col-lg-10";
     return (
-        <SlideProvider>
-            <RecipeProvider>
-                <ChatProvider>
-                    <BaseHeader />
-                    <div className="container">
-                        <div className="row mt-3 ">
-                            <div className={leftSideClasses}>
-                                {!params?.recipe && (
-                                    <>
-                                        <Filter />
-                                        <FilterMobile />
-                                    </>
-                                )}
-                            </div>
-                            <div className={rightSideClasses}>
-                                <Main>
-                                    <Outlet />
-                                </Main>
-                            </div>
-                        </div>
-                    </div>
-                    {isAuthenticated && <Chat />}
-                </ChatProvider>
-            </RecipeProvider>
-        </SlideProvider>
+        <RecipeProvider>
+            <ModalProvider>
+                <SlideProvider>
+                    <ChatProvider>
+                        <Outlet />
+                        <CreateRecipeModal />
+                    </ChatProvider>
+                </SlideProvider>
+            </ModalProvider>
+        </RecipeProvider>
     );
 }
 
