72fd8583fd8b9f390cd5220a5a189481cff2dc83
John <johnsuyang2118@gmail.com>
2025-08-20 22:13:20 +0800

    add missing imports

--- src/layout/base.jsx
+++ src/layout/base.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router";
 import SlideProvider from "../providers/slideProvider";
 import RecipeProvider from "../providers/recipeProvider";
 import ChatProvider from "../providers/chatProvider";
+import ModalProvider from "../providers/modalProvider";
+import CreateRecipeModal from "../components/modals/createNewRecipe/createRecipeModal";
 
 function Base() {
     return (
