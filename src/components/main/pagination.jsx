9ee31b0fdcf0f09d1058af19f74dd98ea4b3ec5d
John <johnsuyang2118@gmail.com>
2025-08-23 18:50:08 +0800

    hide the first and last page when the current active page is far from it

--- src/components/main/pagination.jsx
+++ src/components/main/pagination.jsx
@@ -1,8 +1,13 @@
 import RcPagination from "rc-pagination";
 import LeftCaretIcon from "../../assets/icons/leftCaretIcon";
 import RightCaretIcon from "../../assets/icons/rightCaretIcon";
+import { useState } from "react";
 
 function Pagination({ pageSize = 6, total = 6, currentPage = 1 }) {
+    const [makeFirstPageHidden, setMakeFirstPageHidden] = useState(false);
+    const [makeLastPageHidden, setMakeLastPageHidden] = useState(true);
+    const lastPage = Math.ceil(total / pageSize);
+    const firstPage = 1;
     return (
         <div className="d-flex w-100 justify-content-center">
             <RcPagination
@@ -13,6 +18,36 @@ function Pagination({ pageSize = 6, total = 6, currentPage = 1 }) {
                 className="pagination"
                 prevIcon={LeftCaretIcon}
                 nextIcon={RightCaretIcon}
+                showLessItems
+                showPrevNextJumpers={false}
+                itemRender={(current, type, element) => {
+                    // if the differnce of the current and active element is less than 2 show if, else then hide it
+                    if (element?._owner?.pendingProps?.active && current != firstPage) {
+                        if (current - firstPage < 3) {
+                            setMakeFirstPageHidden(false);
+                        } else {
+                            setMakeFirstPageHidden(true);
+                        }
+                    }
+
+                    if (current == firstPage && element.type === "a" && makeFirstPageHidden) {
+                        return null;
+                    }
+
+                    if (element?._owner?.pendingProps?.active && current != lastPage) {
+                        if (lastPage - current < 3) {
+                            setMakeLastPageHidden(false);
+                        } else {
+                            setMakeLastPageHidden(true);
+                        }
+                    }
+
+                    if (current == lastPage && element.type === "a" && makeLastPageHidden) {
+                        return null;
+                    }
+
+                    return element;
+                }}
             />
         </div>
     );
