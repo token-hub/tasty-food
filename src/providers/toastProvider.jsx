37f889aa5d3e08c35bc29cf26cbbfa66b4349fb2
John <johnsuyang2118@gmail.com>
2025-09-02 20:21:57 +0800

    create toast provider

---
+++ src/providers/toastProvider.jsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState, useCallback } from "react";
+import Toast from "../components/toast/toast";
+
+const ToastContext = createContext({
+    updateToast: () => {},
+    removeToast: () => {},
+    createToast: () => {}
+});
+
+export function useToastContext() {
+    return useContext(ToastContext);
+}
+
+function ToastProvider(data) {
+    const [toasts, setToasts] = useState([]);
+
+    const updateToast = useCallback((id) => {
+        setToasts((toasts) => {
+            return toasts.map((toast) => {
+                if (toast.id === id) {
+                    return { ...toast, shouldClose: true };
+                } else {
+                    return toast;
+                }
+            });
+        });
+    }, []);
+
+    const removeToast = useCallback((id) => {
+        setToasts((toasts) => {
+            return toasts.filter((toast) => toast.id !== id);
+        });
+    }, []);
+
+    const createToast = useCallback(
+        ({ headerText, bodyText }) => {
+            const isExist = toasts.some((toast) => toast.headerText === headerText && toast.bodyText);
+
+            if (!isExist) {
+                setToasts((toasts) => {
+                    return [
+                        ...toasts,
+                        {
+                            id: toasts.length,
+                            headerText,
+                            bodyText
+                        }
+                    ];
+                });
+            }
+        },
+        [toasts]
+    );
+
+    const values = {
+        updateToast,
+        removeToast,
+        createToast
+    };
+
+    return (
+        <ToastContext value={values}>
+            <>
+                <div aria-live="polite" aria-atomic="true" className="position-relative">
+                    <div class="toast-container top-0 end-0 mt-4 me-3">
+                        {toasts.length > 0 &&
+                            toasts.map((toast) => {
+                                return <Toast key={toast.id} toast={toast} />;
+                            })}
+                    </div>
+                </div>
+                {data?.children}
+            </>
+        </ToastContext>
+    );
+}
+
+export default ToastProvider;
