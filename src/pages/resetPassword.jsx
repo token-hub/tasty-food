f988f0d6a40ac91bd5e7de311c5acb6fa4965c2b
John <johnsuyang2118@gmail.com>
2025-09-05 07:39:37 +0800

    create reset password page

---
+++ src/pages/resetPassword.jsx
@@ -0,0 +1,62 @@
+import { Form, useActionData, useSearchParams } from "react-router";
+import AuthSubmitButton from "../components/auth/authSubmitButton";
+import { useToastContext } from "../providers/toastProvider";
+
+function ResetPassword() {
+    const actionData = useActionData();
+    const { createToast } = useToastContext();
+    const [searchParams] = useSearchParams();
+    const token = searchParams.get("token");
+
+    if (!token) {
+        createToast({ headerText: "Invalid or missing token", bodyText: "Kindly start with forgot password again", isSuccess: false });
+    }
+
+    if (actionData?.errors) {
+        actionData.errors.map((error) => createToast({ headerText: "Invalid fields", bodyText: error, isSuccess: false }));
+    }
+
+    return (
+        <div className="auth bg-secondary ">
+            <div className="container h-100">
+                <div className="d-flex justify-content-center align-items-center align-items-sm-baseline h-100 pt-sm-7">
+                    <div className="card" style={{ width: "25rem" }}>
+                        <div className="card-body">
+                            <h6 className="card-title mb-4">Reset password</h6>
+
+                            <Form method="POST" noValidate>
+                                <input type="text" className="d-none" name="token" value={token} />
+                                <div className="form-floating w-100">
+                                    <input
+                                        type="text"
+                                        className="form-control mb-3"
+                                        id="password"
+                                        name="password"
+                                        placeholder="name@example.com"
+                                        required
+                                    />
+                                    <label htmlFor="password">Password</label>
+                                </div>
+                                <div className="form-floating w-100">
+                                    <input
+                                        type="text"
+                                        className="form-control mb-3"
+                                        id="confirm-password"
+                                        name="confirm-password"
+                                        placeholder="name@example.com"
+                                        required
+                                    />
+                                    <label htmlFor="confirm-password">Confirm password</label>
+                                </div>
+
+                                <AuthSubmitButton text="Reset password" disabled={!token} />
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResetPassword;
