8ac3f8ad9eee36f3f9779da9426f75f583f32bcb
John <johnsuyang2118@gmail.com>
2025-09-04 18:10:45 +0800

    Only show verification button when the user has unverified email

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,4 +1,14 @@
+import { useSubmit } from "react-router";
+import { useUserContext } from "../providers/userProvider";
+
 function Profile() {
+    const submit = useSubmit();
+    const { user } = useUserContext();
+
+    function handleEmailVerification() {
+        submit({ email: user.email }, { method: "POST", action: "/emailVerification" });
+    }
+
     return (
         <form className="form-floating p-3 p-md-0" action="" method="POST">
             <div className="form-floating w-100">
@@ -23,7 +33,13 @@ function Profile() {
                 <label htmlFor="lastName">Last name</label>
             </div>
 
-            <div className="d-flex justify-content-end">
+            <div className={`d-flex ${user?.emailVerified ? "justify-content-end" : "justify-content-between"} `}>
+                {!user?.emailVerified && (
+                    <button className="btn btn-primary text-white" type="button" onClick={handleEmailVerification}>
+                        Verify your email
+                    </button>
+                )}
+
                 <button type="submit" className="btn btn-primary text-light">
                     Submit
                 </button>
