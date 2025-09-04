ab5845f527cdbaff91f19a4d293095b4045701b5
John <johnsuyang2118@gmail.com>
2025-09-04 18:09:25 +0800

    remove unnessary providers on auth component

--- src/layout/auth.jsx
+++ src/layout/auth.jsx
@@ -1,15 +1,7 @@
 import { Outlet } from "react-router";
 
-// Providers
-import RecipeProvider from "../providers/recipeProvider";
-import ModalProvider from "../providers/modalProvider";
-import SlideProvider from "../providers/slideProvider";
-import ChatProvider from "../providers/chatProvider";
-
 // Components
-import CreateRecipeModal from "../components/modals/createNewRecipe/createRecipeModal";
 import Chat from "../components/main/chat/chat";
-import MobileSlide from "../components/slides/mobileSlide";
 import BaseHeader from "./baseHeader";
 import Sidebar from "../components/sidebar/sidebar";
 import MobileFooter from "./mobileFooter";
@@ -17,36 +9,28 @@ import Main from "./main";
 
 function AuthLayout() {
     return (
-        <RecipeProvider>
-            <ModalProvider>
-                <SlideProvider>
-                    <ChatProvider>
-                        <BaseHeader />
-                        <div className="container">
-                            <div className="row mt-3 ">
-                                <div className="col-md-3 col-lg-2">
-                                    <div className="d-none d-md-block">
-                                        <Sidebar />
-                                    </div>
-                                </div>
-                                <div className="col-md-9 col-lg-10">
-                                    <Main>
-                                        <Outlet />
-                                    </Main>
-                                </div>
-                            </div>
-                            <Chat />
+        <>
+            <BaseHeader />
+            <div className="container">
+                <div className="row mt-3 ">
+                    <div className="col-md-3 col-lg-2">
+                        <div className="d-none d-md-block">
+                            <Sidebar />
                         </div>
+                    </div>
+                    <div className="col-md-9 col-lg-10">
+                        <Main>
+                            <Outlet />
+                        </Main>
+                    </div>
+                </div>
+                <Chat />
+            </div>
 
-                        <div className="d-md-none ">
-                            <MobileFooter />
-                        </div>
-
-                        <CreateRecipeModal />
-                    </ChatProvider>
-                </SlideProvider>
-            </ModalProvider>
-        </RecipeProvider>
+            <div className="d-md-none ">
+                <MobileFooter />
+            </div>
+        </>
     );
 }
 
