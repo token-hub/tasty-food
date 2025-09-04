b523591d0359c91e5246e8e78353ff6406086b1d
John <johnsuyang2118@gmail.com>
2025-09-05 07:39:47 +0800

    create reset password action

---
+++ src/actions/resetPassword.js
@@ -0,0 +1,32 @@
+import { customFetch, customTryCatchWrapper } from "../lib/utilities";
+import { data as responseData } from "react-router";
+
+export async function resetPasswordAction({ request }) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData.entries());
+    let errors = [];
+
+    if (!data.password || !data["confirm-password"]) {
+        errors.push("New password and confirm password must not be empty ");
+    }
+
+    if (data.password && data["confirm-password"] && data.password !== data["confirm-password"]) {
+        errors.push("New password and confirm password must match");
+    }
+
+    if (errors.length) {
+        return responseData(
+            { errors },
+            {
+                status: 400
+            }
+        );
+    }
+
+    return await customTryCatchWrapper(() => {
+        return customFetch({
+            url: "auth/reset-password",
+            data
+        });
+    });
+}
