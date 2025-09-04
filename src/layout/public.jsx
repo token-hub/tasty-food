31070e4bf8b314d91bf731eddf174231414bbf76
John <johnsuyang2118@gmail.com>
2025-09-04 22:12:55 +0800

    update identifier used for showing chat

--- src/layout/public.jsx
+++ src/layout/public.jsx
@@ -2,11 +2,12 @@ import { Outlet, useParams } from "react-router";
 import BaseHeader from "./baseHeader";
 import Main from "./main";
 import Chat from "../components/main/chat/chat";
-import { isAuthenticated } from "../lib/constants";
 import Filter from "../components/sidebar/filter";
 import FilterMobile from "../components/sidebar/filterMobile";
+import { useUserContext } from "../providers/userProvider";
 
 function Public() {
+    const { user } = useUserContext;
     const params = useParams();
     const leftSideClasses = params?.recipe ? "col-md-1" : "col-md-3 col-lg-2";
     const rightSideClasses = params?.recipe ? "col-md-10" : "col-md-9 col-lg-10";
@@ -30,7 +31,7 @@ function Public() {
                     </div>
                 </div>
             </div>
-            {isAuthenticated && <Chat />}
+            {user && <Chat />}
         </>
     );
 }
