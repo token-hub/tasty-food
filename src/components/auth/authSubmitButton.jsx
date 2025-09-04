e775bf537f07ab09fdeca578fe4441660917f4e8
John <johnsuyang2118@gmail.com>
2025-09-05 07:41:12 +0800

    allow submit button to be more customizable

--- src/components/auth/authSubmitButton.jsx
+++ src/components/auth/authSubmitButton.jsx
@@ -1,13 +1,15 @@
 import { useNavigation } from "react-router";
 
-function AuthSubmitButton() {
+function AuthSubmitButton({ text, disabled }) {
     let navigation = useNavigation();
     return navigation.state === "submitting" ? (
         <button className="w-100 btn btn-sm btn-primary mb-2 text-white" disabled>
             Submitting ...
         </button>
     ) : (
-        <button className="w-100 btn btn-sm btn-primary mb-2 text-white">SUBMIT</button>
+        <button disabled={disabled} className="w-100 btn btn-sm btn-primary mb-2 text-white">
+            {text ?? "Submit"}
+        </button>
     );
 }
 
