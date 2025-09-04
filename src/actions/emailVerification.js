31a9a763916d436308834c9b3684d80161256516
John <johnsuyang2118@gmail.com>
2025-09-04 18:08:26 +0800

    add emailVerification query

---
+++ src/actions/emailVerification.js
@@ -0,0 +1,28 @@
+import { data } from "react-router";
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function emailVerification({ request }) {
+    try {
+        const fields = await request.formData();
+
+        if (!fields.email) return;
+
+        const result = await fetch(`${SERVER_API_URL}/auth/sendEmailVerification`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(fields)
+        });
+
+        await result.json();
+    } catch (error) {
+        return data(
+            {
+                error: error?.message || "Something went wrong"
+            },
+            { status: 500 }
+        );
+    }
+}
