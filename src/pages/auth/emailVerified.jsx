007584eeb9e4347d176af62283923c07cd80b5f1
John <johnsuyang2118@gmail.com>
2025-09-05 09:22:26 +0800

    add page for successfully veritying email

---
+++ src/pages/auth/emailVerified.jsx
@@ -0,0 +1,19 @@
+function EmailVerified() {
+    return (
+        <div className="auth bg-secondary ">
+            <div className="container h-100">
+                <div className="d-flex justify-content-center align-items-center align-items-sm-baseline h-100 pt-sm-7">
+                    <div className="card" style={{ width: "25rem" }}>
+                        <div className="card-body">
+                            <h6 className="card-title mb-4">Email Verified</h6>
+                            <hr className="mt-0" />
+                            <p className="lead text-center">Email successfully verified, you may close this page</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EmailVerified;
