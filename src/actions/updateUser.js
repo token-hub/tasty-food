f263d0bac884f8eb897c33a9e08e3cf887a4102d
John <johnsuyang2118@gmail.com>
2025-09-05 10:57:21 +0800

    create update user aciton

---
+++ src/actions/updateUser.js
@@ -0,0 +1,29 @@
+import { customFetch, customTryCatchWrapper } from "../lib/utilities";
+import { queryClient } from "../lib/queryClient";
+import { data as responseData } from "react-router";
+
+export async function updateUserAction({ request }) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData.entries());
+
+    if (!data.name) {
+        return responseData(
+            { error: "Name must not be empty" },
+            {
+                status: 400
+            }
+        );
+    }
+
+    return await customTryCatchWrapper(
+        () => {
+            return customFetch({
+                url: "auth/updateUser",
+                data
+            });
+        },
+        () => {
+            queryClient.invalidateQueries({ queryKey: ["session"] });
+        }
+    );
+}
