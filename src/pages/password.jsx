5355873220c80dd7c5f5a87bb1e3d940ea6fe0d4
John <johnsuyang2118@gmail.com>
2025-09-05 10:56:29 +0800

    update password page to change password in the server

--- src/pages/password.jsx
+++ src/pages/password.jsx
@@ -1,27 +1,38 @@
+import { Form, useActionData } from "react-router";
+import { useToastContext } from "../providers/toastProvider";
+import { useEffect, useRef } from "react";
+
 function Password() {
+    const actionData = useActionData();
+    const { createToast } = useToastContext();
+    const formRef = useRef();
+
+    useEffect(() => {
+        if (actionData?.errors) {
+            actionData.errors.forEach((error) => createToast({ headerText: "Invalid fields", bodyText: error, isSuccess: false }));
+            formRef.current.reset();
+        }
+
+        if (actionData?.error) {
+            createToast({ headerText: "Server Error", bodyText: actionData?.error, isSuccess: false });
+            formRef.current.reset();
+        }
+
+        if (actionData?.result) {
+            createToast({ headerText: "Change Password", bodyText: "Password was successfully changed" });
+            formRef.current.reset();
+        }
+    }, [actionData, createToast]);
+
     return (
-        <form className="form-floating p-3 p-md-0" action="" method="POST">
+        <Form ref={formRef} className="form-floating p-3 p-md-0" method="POST">
             <div className="form-floating w-100">
-                <input
-                    type="password"
-                    required
-                    className="form-control bg-light mb-3"
-                    id="oldPassword"
-                    defaultValue={"********"}
-                    placeholder="********"
-                />
+                <input type="password" required className="form-control bg-light mb-3" id="oldPassword" name="oldPassword" placeholder="********" />
                 <label htmlFor="oldPassword">Old password</label>
             </div>
 
             <div className="form-floating w-100">
-                <input
-                    type="password"
-                    required
-                    className="form-control bg-light mb-3"
-                    id="newPassword"
-                    defaultValue={"********"}
-                    placeholder="********"
-                />
+                <input type="password" required className="form-control bg-light mb-3" id="newPassword" name="password" placeholder="********" />
                 <label htmlFor="newPassword">New password</label>
             </div>
 
@@ -31,7 +42,7 @@ function Password() {
                     required
                     className="form-control bg-light mb-3"
                     id="confirmPassword"
-                    defaultValue={"********"}
+                    name="confirmPassword"
                     placeholder="********"
                 />
                 <label htmlFor="confirmPassword">Confirm password</label>
@@ -42,7 +53,7 @@ function Password() {
                     Submit
                 </button>
             </div>
-        </form>
+        </Form>
     );
 }
 
