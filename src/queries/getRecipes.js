0a7537f9953a2bf35db2f24d07193332e9444ad2
John <johnsuyang2118@gmail.com>
2025-09-06 12:44:40 +0800

    create fetch for fetching recipes

---
+++ src/queries/getRecipes.js
@@ -0,0 +1,31 @@
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function getRecipes({ signal, pagination = {}, filters = { categories: [] } }) {
+    const result = await fetch(`${SERVER_API_URL}/recipes/getRecipes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        signal,
+        credentials: "include",
+        body: JSON.stringify({
+            pagination: {
+                page: pagination.page ?? 1,
+                cursor: pagination.cursor ?? "",
+                limit: pagination.limit ?? 6,
+                sortBy: pagination.sortBy ?? "updatedAt"
+            },
+            filters: {
+                categories: filters.categories ?? []
+            }
+        })
+    });
+
+    const responseData = await result.json();
+
+    if (result.ok) {
+        return responseData;
+    } else {
+        throw new Error(responseData.error || "Something went wrong");
+    }
+}
