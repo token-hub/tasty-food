4464b629694c1f9d3c139f0f80c0def7a5172f38
John <johnsuyang2118@gmail.com>
2025-09-06 22:55:28 +0800

    sync the changes in the inputs with the state

--- src/components/modals/createNewRecipe/ingredientsInputs.jsx
+++ src/components/modals/createNewRecipe/ingredientsInputs.jsx
@@ -1,19 +1,48 @@
-import React from "react";
 import XIcon from "../../../assets/icons/xIcon";
 
-function IngredientsInputs({ quantity, unit, name, id, removeIngredient }) {
+function IngredientsInputs({ quantity, unit, name, id, onChange, removeIngredient }) {
     return (
         <div className="d-flex w-100 align-items-center h-100">
             <div className="form-floating w-100">
-                <input type="text" className="form-control mb-3" id="quantity" defaultValue={quantity} placeholder="1" />
+                <input
+                    type="number"
+                    className="form-control mb-3"
+                    id="quantity"
+                    parentid={id}
+                    parenttype="ingredient"
+                    name="quantity"
+                    defaultValue={quantity}
+                    placeholder="1"
+                    onChange={onChange}
+                />
                 <label htmlFor="quantity">Quantity</label>
             </div>
             <div className="form-floating w-100">
-                <input type="text" className="form-control mb-3" id="unit" defaultValue={unit} placeholder="1" />
+                <input
+                    type="text"
+                    className="form-control mb-3"
+                    id="unit"
+                    parentid={id}
+                    parenttype="ingredient"
+                    name="unit"
+                    defaultValue={unit}
+                    placeholder="1"
+                    onChange={onChange}
+                />
                 <label htmlFor="unit">Unit</label>
             </div>
             <div className="form-floating w-100">
-                <input type="text" className="form-control mb-3" id="ingredient" defaultValue={name} placeholder="1" />
+                <input
+                    type="text"
+                    className="form-control mb-3"
+                    id="ingredient"
+                    parenttype="ingredient"
+                    parentid={id}
+                    name="ingredient"
+                    defaultValue={name}
+                    placeholder="1"
+                    onChange={onChange}
+                />
                 <label htmlFor="ingredient">Ingredient</label>
             </div>
             <button type="button" onClick={() => removeIngredient(id)} className="btn text-secondary mb-3">
