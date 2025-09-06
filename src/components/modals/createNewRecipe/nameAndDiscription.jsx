68fb12ae86f014224da548d3285947e17b190227
John <johnsuyang2118@gmail.com>
2025-09-06 18:12:30 +0800

    pass the onchange to the name and Discription component

--- src/components/modals/createNewRecipe/nameAndDiscription.jsx
+++ src/components/modals/createNewRecipe/nameAndDiscription.jsx
@@ -1,13 +1,22 @@
-function NameAndDiscription({ recipe }) {
+function NameAndDiscription({ recipe, onChange }) {
     return (
         <>
             <div className="form-floating">
-                <input type="text" defaultValue={recipe.name} className="form-control mb-3" id="name" placeholder="name" name="" />
+                <input
+                    type="text"
+                    onChange={onChange}
+                    defaultValue={recipe.name}
+                    className="form-control mb-3"
+                    id="name"
+                    placeholder="name"
+                    name=""
+                />
                 <label htmlFor="name">Recipe name</label>
             </div>
 
             <div className="form-floating">
                 <textarea
+                    onChange={onChange}
                     className="form-control mb-3"
                     style={{ height: "10rem" }}
                     placeholder="Leave a comment here"
