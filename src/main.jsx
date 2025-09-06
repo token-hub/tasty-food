111395cc0124d54594fbc2ba958e6b694a403a20
John <johnsuyang2118@gmail.com>
2025-09-06 12:41:45 +0800

    move the providers to wrapped the main so that useQuery works on base path

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,31 @@ import "rc-pagination/assets/index.css";
 import { RouterProvider } from "react-router";
 import router from "./router.js";
 
+import { queryClient } from "./lib/queryClient.js";
+import { QueryClientProvider } from "@tanstack/react-query";
+import SlideProvider from "./providers/slideProvider";
+import RecipeProvider from "./providers/recipeProvider";
+import ChatProvider from "./providers/chatProvider";
+import ModalProvider from "./providers/modalProvider";
+import ToastProvider from "./providers/toastProvider";
+import UserProvider from "./providers/userProvider";
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                <RecipeProvider>
+                    <ModalProvider>
+                        <SlideProvider>
+                            <ChatProvider>
+                                <ToastProvider>
+                                    <RouterProvider router={router} />
+                                </ToastProvider>
+                            </ChatProvider>
+                        </SlideProvider>
+                    </ModalProvider>
+                </RecipeProvider>
+            </UserProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
