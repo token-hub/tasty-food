dbbc3c2144b3207519ed187246bc638c56a2d56a
John <johnsuyang2118@gmail.com>
2025-09-07 00:31:43 +0800

    sync the change in the input with the state and add video preview

--- src/components/modals/createNewRecipe/uploadVideo.jsx
+++ src/components/modals/createNewRecipe/uploadVideo.jsx
@@ -1,10 +1,32 @@
-function CreateModalUploadVideo() {
+import { useState, useRef } from "react";
+
+function CreateModalUploadVideo({ recipe, onChange }) {
+    const [video, setVideo] = useState(recipe?.video?.link ?? null);
+    const videoInputRef = useRef();
+
+    function handleUpload(e) {
+        const file = videoInputRef.current.files[0];
+
+        if (file && file.type.startsWith("video/")) {
+            const url = URL.createObjectURL(file);
+            setVideo(url);
+        }
+
+        onChange(e);
+    }
+
     return (
         <div className="my-3">
             <label htmlFor="formFile" className="form-label">
                 Upload video instruction
             </label>
-            <input className="form-control" name="recipeVideo" type="file" id="formFile" />
+            <input ref={videoInputRef} onChange={handleUpload} className="form-control" name="video" type="file" accept="video/*" id="formFile" />
+
+            {video && (
+                <div className="w-100 vh-50 my-lg-3">
+                    <video src={video} id="videoPreview" controls className="w-100"></video>
+                </div>
+            )}
         </div>
     );
 }
