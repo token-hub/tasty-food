822120d0364188f06595633ffaf345eac3f4781a
John <johnsuyang2118@gmail.com>
2025-09-07 00:02:06 +0800

    sync the changes in the input with the state

--- src/components/modals/createNewRecipe/instructionInput.jsx
+++ src/components/modals/createNewRecipe/instructionInput.jsx
@@ -1,9 +1,18 @@
 import XIcon from "../../../assets/icons/xIcon";
-function InstructionInput({ instruction, id, removeInstruction, index }) {
+
+function InstructionInput({ instruction, id, index, onChange, removeInstruction }) {
     return (
         <div className="d-flex">
             <div className="form-floating w-100">
-                <input type="text" className="form-control mb-3" id="unit" defaultValue={instruction} placeholder="1" />
+                <input
+                    type="text"
+                    className="form-control mb-3"
+                    id={id}
+                    name="instruction"
+                    defaultValue={instruction}
+                    onChange={onChange}
+                    placeholder="1"
+                />
                 <label htmlFor="unit">{`instruction #${index + 1}`}</label>
             </div>
             <button type="button" className="btn text-secondary mb-3" onClick={() => removeInstruction(id)}>
