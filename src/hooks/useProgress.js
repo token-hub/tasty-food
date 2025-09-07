a40c3d016f1a8b9d10e4cf4d7ff64f308a29cc21
John <johnsuyang2118@gmail.com>
2025-09-07 12:30:54 +0800

    show errors in toast when moving to next while there's still required fields

--- src/hooks/useProgress.js
+++ src/hooks/useProgress.js
@@ -1,6 +1,22 @@
 import { useState, useEffect } from "react";
+import { useToastContext } from "../providers/toastProvider";
+import { validateRecipeState } from "../lib/utilities";
 
-export function useProgress(show) {
+export function useProgress(show, recipeState) {
+    const { name, description, cookTime, prepTime, categories, ingredients, instructions } = recipeState;
+    const { createToast } = useToastContext();
+    const toValidate = {
+        1: {
+            name,
+            description,
+            cookMinutes: cookTime.minutes,
+            prepMinutes: prepTime.minutes,
+            categories
+        },
+        2: { ingredients },
+        3: { instructions }
+    };
+
     const progressStep = 25; // for 4 parts of the modal that will sum to 100
     const progressMax = 100;
     const [progress, setProgress] = useState(progressStep);
@@ -17,7 +33,22 @@ export function useProgress(show) {
         }
     }, [show, progress]);
 
+    function handleErrors() {
+        const current = [firstPart, secondPart, thirdPart, fourthPart].findIndex((d) => d);
+        const errors = validateRecipeState(toValidate[current + 1]);
+
+        if (errors.length) {
+            errors.forEach((error) => {
+                createToast({ headerText: "Field Error", bodyText: error.message, isSuccess: false });
+            });
+        }
+
+        return Array.isArray(errors) ? errors.length : errors;
+    }
+
     function handleNext() {
+        const hasError = handleErrors();
+        if (hasError) return;
         setProgress((prev) => prev + progressStep);
     }
 
