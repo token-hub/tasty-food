2e96e0815d1338582da858932ff0958009b3cc54
John <johnsuyang2118@gmail.com>
2025-09-07 08:12:08 +0800

    move default recipe state in the constant file and add ids to the sample recipe constant

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -48,6 +48,28 @@ export const SUBMENU_HEADERS = {
     2: "chats"
 };
 
+export const DEFAULT_RECIPE_STATE = {
+    author: {
+        name: "",
+        userId: ""
+    },
+    instructions: [],
+    ingredients: [],
+    categories: [],
+    cookTime: {
+        hours: 0,
+        minutes: 0
+    },
+    prepTime: {
+        hours: 0,
+        minutes: 0
+    },
+    name: "",
+    description: "",
+    image: null,
+    video: null
+};
+
 export const sampleRecipes = [
     {
         author: {
@@ -56,7 +78,9 @@ export const sampleRecipes = [
         },
         name: "Sinigang na baboy",
         recipeLink: "/recipes/sinigang-na-baboy",
-        imageSource: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg",
+        image: {
+            link: "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot-257x257.jpg"
+        },
         goodForPeopleCount: 3,
         prepTime: { hours: 1, minutes: 40 },
         cookTime: { hours: 1, minutes: 0 },
@@ -64,31 +88,35 @@ export const sampleRecipes = [
         description:
             "Thick mung bean stew with calabaza squash, malunggay, spinach, dried shrimp, and crispy pork rinds. A satisfying and nutritious Filipino comfort food perfect with rice.",
         ingredients: [
-            { name: "mung beans", quantity: "14", unit: "ounce/s", isMain: true },
-            { name: "calabaza squash cubed", quantity: " 1", unit: "lb/s", isMain: false },
-            { name: "cup salted dried shrimp hibe", quantity: "1 1/3", unit: "cup/s", isMain: false },
-            { name: "spinach", quantity: "2", unit: "cup/s", isMain: false },
-            { name: "tomatoes diced", quantity: "2", unit: "cup/s", isMain: false },
-            { name: "onion chopped", quantity: "1", unit: "pc/s", isMain: false },
-            { name: "cloves garlic chopped", quantity: "4", unit: "pc/s", isMain: false },
-            { name: "pork rinds chicharon", quantity: "1", unit: "cup/s", isMain: false },
-            { name: "Maggi Magic Sarap", quantity: "8", unit: "gram/s", isMain: false },
-            { name: "fish sauce", quantity: "3", unit: "tablespoon/s", isMain: false },
-            { name: "ground black pepper", quantity: "1/3", unit: "teaspoon/s", isMain: false },
-            { name: "water", quantity: "1 1/3", unit: "quart/s", isMain: false },
-            { name: "cooking oil", quantity: "3", unit: "tablespoon/s", isMain: false }
+            { id: 1, name: "mung beans", quantity: "14", unit: "ounce/s", isMain: true },
+            { id: 2, name: "calabaza squash cubed", quantity: " 1", unit: "lb/s", isMain: false },
+            { id: 3, name: "cup salted dried shrimp hibe", quantity: "1 1/3", unit: "cup/s", isMain: false },
+            { id: 4, name: "spinach", quantity: "2", unit: "cup/s", isMain: false },
+            { id: 5, name: "tomatoes diced", quantity: "2", unit: "cup/s", isMain: false },
+            { id: 6, name: "onion chopped", quantity: "1", unit: "pc/s", isMain: false },
+            { id: 7, name: "cloves garlic chopped", quantity: "4", unit: "pc/s", isMain: false },
+            { id: 8, name: "pork rinds chicharon", quantity: "1", unit: "cup/s", isMain: false },
+            { id: 9, name: "Maggi Magic Sarap", quantity: "8", unit: "gram/s", isMain: false },
+            { id: 10, name: "fish sauce", quantity: "3", unit: "tablespoon/s", isMain: false },
+            { id: 11, name: "ground black pepper", quantity: "1/3", unit: "teaspoon/s", isMain: false },
+            { id: 12, name: "water", quantity: "1 1/3", unit: "quart/s", isMain: false },
+            { id: 13, name: "cooking oil", quantity: "3", unit: "tablespoon/s", isMain: false }
         ],
         instructions: [
-            "Soak the mung beans in ½ quart water overnight. 14 ounces mung beans, 1 ½ quarts water",
-            "Heat 3 tablespoons of cooking oil in a deep cooking pot.",
-            "Sauté the garlic until it starts to brown. Add the onion and tomato. Continue sautéing until softened.",
-            "Add the dried shrimp and soaked mung beans. Stir well.",
-            "Pour in the fish sauce and 1 quart of water. Cover and simmer until the mung beans are completely soft and the soup develops a thick consistency. Note: Add more water as needed.",
-            "Add the calabaza squash. Cover and continue cooking until the squash is tender.",
-            "Add the malunggay leaves and spinach. Cook for 1 minute.",
-            "Season with ground black pepper and Maggi Magic Sarap. Stir well.",
-            "Top with crispy chicharon.",
-            "Serve hot with fried pork belly and steamed rice."
+            { id: 1, instruction: "Soak the mung beans in ½ quart water overnight. 14 ounces mung beans, 1 ½ quarts water" },
+            { id: 2, instruction: "Heat 3 tablespoons of cooking oil in a deep cooking pot." },
+            { id: 3, instruction: "Sauté the garlic until it starts to brown. Add the onion and tomato. Continue sautéing until softened." },
+            { id: 4, instruction: "Add the dried shrimp and soaked mung beans. Stir well." },
+            {
+                id: 5,
+                instruction:
+                    "Pour in the fish sauce and 1 quart of water. Cover and simmer until the mung beans are completely soft and the soup develops a thick consistency. Note: Add more water as needed."
+            },
+            { id: 6, instruction: "Add the calabaza squash. Cover and continue cooking until the squash is tender." },
+            { id: 7, instruction: "Add the malunggay leaves and spinach. Cook for 1 minute." },
+            { id: 8, instruction: "Season with ground black pepper and Maggi Magic Sarap. Stir well." },
+            { id: 9, instruction: "Top with crispy chicharon." },
+            { id: 10, instruction: "Serve hot with fried pork belly and steamed rice." }
         ],
         topFiveRecentRatings: [
             {
