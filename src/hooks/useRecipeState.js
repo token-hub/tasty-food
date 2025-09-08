82714e5fabdd9bc4d7cccb1f907a3084ca663982
John <johnsuyang2118@gmail.com>
2025-09-08 21:33:06 +0800

    move the recipe state into its own hook

---
+++ src/hooks/useRecipeState.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import { getRecipe } from "../queries/getRecipe";
+import { DEFAULT_RECIPE_STATE } from "../lib/constants";
+import { useState } from "react";
+
+export function useRecipeState() {
+    const { recipeId } = useParams();
+
+    const { data } = useQuery({
+        queryKey: ["recipe"],
+        queryFn: ({ signal }) => getRecipe({ signal, recipeId }),
+        enabled: Boolean(recipeId)
+    });
+    const initialState = data?.data ?? DEFAULT_RECIPE_STATE;
+    const [recipeState, setRecipeState] = useState(initialState);
+
+    return { recipeState, setRecipeState, recipeId, data, initialState };
+}
