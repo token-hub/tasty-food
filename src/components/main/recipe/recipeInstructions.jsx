4f4600047141f227c4df7ba71027402a0776a062
John <johnsuyang2118@gmail.com>
2025-09-08 19:21:09 +0800

    remove count 0 showing in ui when theres no instructions

--- src/components/main/recipe/recipeInstructions.jsx
+++ src/components/main/recipe/recipeInstructions.jsx
@@ -3,7 +3,7 @@ function RecipeInstructions({ recipe }) {
         <>
             <h3>Instructions</h3>
             <ol class="list-group list-group-flush list-group-numbered">
-                {recipe.instructions.length &&
+                {recipe.instructions.length > 0 &&
                     recipe.instructions.map((instruction, index) => (
                         <li key={index} class="list-group-item lead py-1">
                             {instruction}
