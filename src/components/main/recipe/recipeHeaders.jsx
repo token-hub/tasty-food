5df6050818876c76bb7e7d4a535b414453828cc3
John <johnsuyang2118@gmail.com>
2025-09-08 19:21:28 +0800

    use default imag when there's no recipe image supplied

--- src/components/main/recipe/recipeHeaders.jsx
+++ src/components/main/recipe/recipeHeaders.jsx
@@ -1,7 +1,7 @@
 import ChatIcon from "../../../assets/icons/chatIcon";
 import ArchiveIcon from "../../../assets/icons/archiveIcon";
 import EditIcon from "../../../assets/icons/editIcon";
-import { isAuthor } from "../../../lib/constants";
+import { DEFAULT_IMAGE, isAuthor } from "../../../lib/constants";
 import { useModalContext } from "../../../providers/modalProvider";
 import { useChatContext } from "../../../providers/chatProvider";
 import { MODAL_MODES } from "../../../lib/constants";
@@ -9,10 +9,12 @@ import { useSlideContext } from "../../../providers/slideProvider";
 import ChatMaximizedBody from "../chat/chatMaximizedBody";
 
 function RecipeHeaders({ recipe }) {
-    let { imageSource, goodForPeopleCount, description } = recipe;
+    let { image, goodForPeopleCount, description, name } = recipe;
     const { setCurrentModal } = useModalContext();
     const { handleOpenChat } = useChatContext();
     const { openSlide } = useSlideContext();
+    const imageSource = image ?? DEFAULT_IMAGE;
+    const alt = image ? name : "default image";
 
     function handleEdit() {
         setCurrentModal("recipe", MODAL_MODES[1]);
@@ -30,7 +32,7 @@ function RecipeHeaders({ recipe }) {
         <div className="row">
             <div className="col-lg-3">
                 <div className="d-flex w-100">
-                    <img src={imageSource} alt="sample" className="img-fluid rounded mb-3 ms-auto me-auto" />
+                    <img src={imageSource} alt={alt} className="img-fluid rounded mb-3 ms-auto me-auto" />
                 </div>
             </div>
             <div className="col-lg-8">
