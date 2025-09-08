ba66aab7ca32a0304f774fe635f165c05121b914
John <johnsuyang2118@gmail.com>
2025-09-08 19:52:31 +0800

    use the recipeId instead of the recipe name as recipeLink

--- src/components/main/recipe/recipeClickable.jsx
+++ src/components/main/recipe/recipeClickable.jsx
@@ -1,14 +1,12 @@
 import { Link, useLocation } from "react-router";
-import { useRecipeContext } from "../../../providers/recipeProvider";
 import { removeSpacesFromText } from "../../../lib/utilities";
 import { DEFAULT_IMAGE } from "../../../lib/constants";
 
 function Recipe({ recipe, isArchived = false }) {
     const { image, name } = recipe;
-    const { setCurrentRecipe } = useRecipeContext();
     const { pathname } = useLocation();
     const linkPreFix = pathname.includes("me") ? "me" : removeSpacesFromText(recipe.author.name);
-    const recipeLinkToUse = "/" + linkPreFix + "/recipes/" + name;
+    const recipeLinkToUse = `/${linkPreFix}/recipes/${recipe._id}`;
     const imageSource = image ?? DEFAULT_IMAGE;
 
     let classes = "card recipe bg-white shadow ";
@@ -16,13 +14,9 @@ function Recipe({ recipe, isArchived = false }) {
         classes += "archived";
     }
 
-    function setRecipe() {
-        setCurrentRecipe(recipe);
-    }
-
     return (
         <div className={classes}>
-            <Link to={recipeLinkToUse} onClick={setRecipe} className="text-muted link-underline link-underline-opacity-0">
+            <Link to={recipeLinkToUse} className="text-muted link-underline link-underline-opacity-0">
                 <div className="recipe_img">
                     <img src={imageSource} className="card-img-top " alt="test image" />
                 </div>
