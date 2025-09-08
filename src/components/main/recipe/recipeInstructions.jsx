eb5da60b5132b4d1e3d8590dfadec0d161be22f8
John <johnsuyang2118@gmail.com>
2025-09-08 21:34:43 +0800

    update the map of instructions due to data being an object

--- src/components/main/recipe/recipeInstructions.jsx
+++ src/components/main/recipe/recipeInstructions.jsx
@@ -4,9 +4,9 @@ function RecipeInstructions({ recipe }) {
             <h3>Instructions</h3>
             <ol class="list-group list-group-flush list-group-numbered">
                 {recipe.instructions.length > 0 &&
-                    recipe.instructions.map((instruction, index) => (
-                        <li key={index} class="list-group-item lead py-1">
-                            {instruction}
+                    recipe.instructions.map((instruction) => (
+                        <li key={instruction.id} class="list-group-item lead py-1">
+                            {instruction.instruction}
                         </li>
                     ))}
             </ol>
