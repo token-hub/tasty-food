5cc7023e4d1e0e33fa924fac9f8e0b496662f579
John <johnsuyang2118@gmail.com>
2025-09-09 01:49:07 +0800

    reuse getOwnRecipes to get also only those archived recipes

--- src/queries/getOwnRecipes.js
+++ src/queries/getOwnRecipes.js
@@ -1,6 +1,6 @@
 import { SERVER_API_URL } from "../lib/constants";
 
-export async function getOwnRecipes({ signal, pagination = {}, author }) {
+export async function getOwnRecipes({ signal, pagination = {}, author, isArchived = false }) {
     const result = await fetch(`${SERVER_API_URL}/recipes/getRecipes`, {
         method: "POST",
         headers: {
@@ -15,7 +15,8 @@ export async function getOwnRecipes({ signal, pagination = {}, author }) {
                 limit: pagination.limit ?? 6,
                 sortBy: pagination.sortBy ?? "updatedAt"
             },
-            author
+            author,
+            isArchived
         })
     });
 
