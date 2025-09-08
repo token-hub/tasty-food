9618994c2f31f9d035ba06e505909044d52273e2
John <johnsuyang2118@gmail.com>
2025-09-08 11:41:51 +0800

    add a fetch method for fetch current user own recipes

---
+++ src/queries/getOwnRecipes.js
@@ -0,0 +1,29 @@
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function getOwnRecipes({ signal, pagination = {}, author }) {
+    const result = await fetch(`${SERVER_API_URL}/recipes/getRecipes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        signal,
+        credentials: "include",
+        body: JSON.stringify({
+            pagination: {
+                page: pagination.page ?? 1,
+                cursor: pagination.cursor ?? "",
+                limit: pagination.limit ?? 6,
+                sortBy: pagination.sortBy ?? "updatedAt"
+            },
+            author
+        })
+    });
+
+    const responseData = await result.json();
+
+    if (result.ok) {
+        return responseData;
+    } else {
+        throw new Error(responseData.error || "Something went wrong");
+    }
+}
