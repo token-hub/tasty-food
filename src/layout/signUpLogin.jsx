4a3f309cc5e5a493b9cc8fe73c9157348494cabb
John <johnsuyang2118@gmail.com>
2025-09-09 22:26:11 +0800

    if there's authenticated user, navigate them to the home page

--- src/layout/signUpLogin.jsx
+++ src/layout/signUpLogin.jsx
@@ -1,6 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Header from "./authHeader";
+import { useUserContext } from "../providers/userProvider";
+import { useEffect } from "react";
+
 function SignUpLogin() {
+    const { user, isLoading } = useUserContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isLoading && user) {
+            navigate("/");
+        }
+    }, [isLoading, user, navigate]);
+
     return (
         <>
             <Header />
