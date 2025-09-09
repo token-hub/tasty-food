7910e334412164a4a7946d212b0f7d7852c15bfe
John <johnsuyang2118@gmail.com>
2025-09-09 21:32:47 +0800

    enable unarchive recipe

--- src/components/main/recipe/recipeClickable.jsx
+++ src/components/main/recipe/recipeClickable.jsx
@@ -1,9 +1,9 @@
 import { Link, useLocation } from "react-router";
-import { removeSpacesFromText } from "../../../lib/utilities";
+import { objectToFormData, removeSpacesFromText } from "../../../lib/utilities";
 import { DEFAULT_IMAGE } from "../../../lib/constants";
 import UnarchiveIcon from "../../../assets/icons/unarchiveIcon";
 
-function Recipe({ recipe, isArchived = false }) {
+function Recipe({ recipe, isArchived = false, fetcher }) {
     const { image, name } = recipe;
     const { pathname } = useLocation();
     const linkPreFix = pathname.includes("me") ? "me" : removeSpacesFromText(recipe.author.name);
@@ -15,25 +15,34 @@ function Recipe({ recipe, isArchived = false }) {
         classes += "archived";
     }
 
+    function handleUnarchiveRecipe() {
+        fetcher.submit(objectToFormData({ recipeId: recipe._id, isArchive: false }), {
+            method: "PUT",
+            action: "/me/recipes/create"
+        });
+    }
+
     return (
         <div className={classes}>
-            <Link to={recipeLinkToUse} className="text-muted link-underline link-underline-opacity-0">
-                <div className="recipe_img">
+            <div className="recipe_img">
+                <Link to={recipeLinkToUse} className="text-muted link-underline link-underline-opacity-0">
                     <img src={imageSource} className="card-img-top " alt="test image" />
-                </div>
-                <div className="card-body">
-                    <div className="row pe-2">
-                        <div className="col-11">
+                </Link>
+            </div>
+            <div className="card-body">
+                <div className="row pe-2">
+                    <div className="col-11">
+                        <Link to={recipeLinkToUse} className="text-muted link-underline link-underline-opacity-0">
                             <h5 className="card-title mb-3">{name}</h5>
-                        </div>
-                        <div className="col-1">
-                            <button className="btn border-0 p-0">
-                                <UnarchiveIcon className="text-primary" height="16" width="16" />
-                            </button>
-                        </div>
+                        </Link>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn border-0 p-0" onClick={handleUnarchiveRecipe}>
+                            <UnarchiveIcon className="text-primary" height="16" width="16" />
+                        </button>
                     </div>
                 </div>
-            </Link>
+            </div>
         </div>
     );
 }
