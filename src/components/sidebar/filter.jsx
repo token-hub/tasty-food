5412eebf452f93732e532a8fa8f4896e5fa5ca71
John <johnsuyang2118@gmail.com>
2025-09-12 04:04:31 +0800

    sync the filter state with the filter input

--- src/components/sidebar/filter.jsx
+++ src/components/sidebar/filter.jsx
@@ -1,8 +1,16 @@
 import FilterIcon from "../../assets/icons/filterIcon";
 import { RECIPE_CATEGORIES } from "../../lib/constants";
+import { useRecipeFilterContext } from "../../providers/recipeFilterProvider";
 
 function Filter() {
     const categories = Object.values(RECIPE_CATEGORIES);
+    const { filter, handlefilter } = useRecipeFilterContext();
+
+    function handleChange(e) {
+        const id = e.target.id;
+        handlefilter(id);
+    }
+    console.log(filter);
     return (
         <aside className="d-none d-md-block">
             <div className="d-flex align-items-center py-2">
@@ -11,7 +19,14 @@ function Filter() {
             </div>
             {categories.map((category) => (
                 <div className="form-check my-1" key={category}>
-                    <input className="form-check-input border-gray-dark" type="checkbox" defaultValue="" id="checkDefault" />
+                    <input
+                        className="form-check-input border-gray-dark"
+                        type="checkbox"
+                        defaultValue=""
+                        id={category}
+                        checked={filter.includes(category) ? true : false}
+                        onChange={handleChange}
+                    />
                     <label className="form-check-label fs-7 text-capitalize" htmlFor="checkDefault">
                         {category}
                     </label>
