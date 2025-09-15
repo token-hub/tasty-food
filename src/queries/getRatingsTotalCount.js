40766aff3b6993fd84e629d5a037e056c99b9a5d
John <johnsuyang2118@gmail.com>
2025-09-16 06:11:33 +0800

    query function for getting the total ratings count

---
+++ src/queries/getRatingsTotalCount.js
@@ -0,0 +1,20 @@
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function getRatingsTotalCount({ signal, recipeId }) {
+    const result = await fetch(`${SERVER_API_URL}/ratings/${recipeId}/getRatings/totalCount`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        signal,
+        credentials: "include"
+    });
+
+    const responseData = await result.json();
+
+    if (result.ok) {
+        return responseData;
+    } else {
+        throw new Error(responseData.error || "Something went wrong");
+    }
+}
