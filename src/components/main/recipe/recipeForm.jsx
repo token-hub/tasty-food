c1d98bf537149de2c0d46156f019c71cc9126226
John <johnsuyang2118@gmail.com>
2025-09-16 05:16:34 +0800

    always show the likes count

--- src/components/main/recipe/recipeForm.jsx
+++ src/components/main/recipe/recipeForm.jsx
@@ -69,14 +69,12 @@ function RecipeForm() {
             </div>
 
             <div className="d-flex align-items-center mt-2">
-                {rating ? (
-                    <button disabled className="btn d-flex align-items-center p-0 border-0 ms-2">
-                        <div className="d-flex">
-                            <LikeFillIcon className="text-primary" />
-                            <span className="text-muted fs-7 ms-1">{rating?.likes?.length || 0}</span>
-                        </div>
-                    </button>
-                ) : null}
+                <button disabled className="btn d-flex align-items-center p-0 border-0 ms-2">
+                    <div className="d-flex">
+                        <LikeFillIcon className="text-primary" />
+                        <span className="text-muted fs-7 ms-1">{rating?.likes?.length || 0}</span>
+                    </div>
+                </button>
 
                 {!disabled && (
                     <button type="submit" className="btn btn-primary text-white mt-3 ms-auto">
