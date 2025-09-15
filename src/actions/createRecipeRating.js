b3cd4f70a5a55c48e28be62c293871a0e5a515c0
John <johnsuyang2118@gmail.com>
2025-09-16 02:59:18 +0800

    create recipe rating action

---
+++ src/actions/createRecipeRating.js
@@ -0,0 +1,21 @@
+import { customTryCatchWrapper, customFetch, formDataToObject } from "../lib/utilities";
+import { queryClient } from "../lib/queryClient";
+
+export async function createRecipeRatingAction({ request }) {
+    const formData = await request.formData();
+    const data = formDataToObject(formData);
+
+    const result = await customTryCatchWrapper(
+        () => {
+            return customFetch({
+                url: "ratings",
+                data
+            });
+        },
+        async () => {
+            await queryClient.invalidateQueries({ queryKey: ["recipes", "single"], exact: true });
+        }
+    );
+
+    return result;
+}
