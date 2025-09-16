3b7f09a72c1ed25e91b9c8ec625532a8da2dd619
John <johnsuyang2118@gmail.com>
2025-09-16 21:14:42 +0800

    query for getting the ratings of a recipe

---
+++ src/queries/getRatings.js
@@ -0,0 +1,24 @@
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function getRatings({ signal, recipeId, pagination = {} }) {
+    const result = await fetch(`${SERVER_API_URL}/ratings/getRatings`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        signal,
+        credentials: "include",
+        body: JSON.stringify({
+            pagination,
+            recipeId
+        })
+    });
+
+    const responseData = await result.json();
+
+    if (result.ok) {
+        return responseData;
+    } else {
+        throw new Error(responseData.error || "Something went wrong");
+    }
+}
