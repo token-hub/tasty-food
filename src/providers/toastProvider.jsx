4bb65fac0529922029fe27f4ac34098353e2eb9d
John <johnsuyang2118@gmail.com>
2025-09-22 21:29:41 +0800

    fix wrong position of toast

--- src/providers/toastProvider.jsx
+++ src/providers/toastProvider.jsx
@@ -65,8 +65,8 @@ function ToastProvider(data) {
     return (
         <ToastContext value={values}>
             <>
-                <div aria-live="polite" aria-atomic="true" className="position-relative">
-                    <div className="toast-container top-0 end-0 mt-4 me-3">
+                <div aria-live="polite" aria-atomic="true" className="position-fixed w-100 z-3">
+                    <div className="toast-container top-0 end-0 mt-4 me-md-3">
                         {toasts.length > 0 &&
                             toasts.map((toast) => {
                                 return <Toast key={toast.id} toast={toast} />;
