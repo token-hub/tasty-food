b7dcf09146ea58ff9b25f8c226490c1965ea62f8
John <johnsuyang2118@gmail.com>
2025-09-24 04:32:33 +0800

    get and show conversations

--- src/components/main/chat/chatMaximized.jsx
+++ src/components/main/chat/chatMaximized.jsx
@@ -1,9 +1,21 @@
 import ChatConvo from "./chatConvo";
 import ChatMaximizedHeader from "./chatMaximizedHeader";
 import ChatMaximizedBody from "./chatMaximizedBody";
+import { useQuery } from "@tanstack/react-query";
+import { getConversations } from "../../../queries/getConversations";
+import { useUserContext } from "../../../providers/userProvider";
+import { usePagination } from "../../../hooks/usePagination";
 
 function ChatMaximized({ chatCount, isOpen, onClick }) {
     let hasSelectedConvo = true;
+    const { user } = useUserContext();
+    const { pagination } = usePagination();
+
+    const { data: { details = [] } = {}, isLoading } = useQuery({
+        queryKey: ["chat", "conversations"],
+        queryFn: ({ signal }) => getConversations({ signal, userId: user?.id, pagination }),
+        enabled: Boolean(isOpen && user?.id)
+    });
 
     return (
         <div className={`chat-maximized position-fixed bottom-0 end-0 bg-light rounded-top shadow ${isOpen ? "chat-maximized-open" : ""} `}>
@@ -11,7 +23,21 @@ function ChatMaximized({ chatCount, isOpen, onClick }) {
             <div className="chat-body">
                 <div className="row h-100">
                     <div className="col-4 h-100 border-end pe-0 overflow-auto">
-                        <ChatConvo name="john doe doe doe" date="2022/11/17" text="very important message" convoCount={5} />
+                        {isLoading && (
+                            <p className="text-center">
+                                <span className="spinner-grow spinner-grow-sm me-2" aria-hidden="true" />
+                                <span className="text-muted fs-7">Loading ...</span>
+                            </p>
+                        )}
+
+                        {!isLoading &&
+                            details.length > 0 &&
+                            details.map(({ _id, participants, updatedAt, messages }) => {
+                                const convoWith = participants.find((u) => u.userId != user.id);
+                                const text = messages[0] ?? "...";
+                                const convoCount = messages.filter((m) => !m.isRead).length;
+                                return <ChatConvo name={convoWith.name} date={updatedAt} key={_id} text={text} convoCount={convoCount} />;
+                            })}
                     </div>
                     <div className="col-8 h-100 ps-0 ">
                         {!hasSelectedConvo && (
---
+++ src/queries/getConversations.js
@@ -0,0 +1,24 @@
+import { SERVER_API_URL } from "../lib/constants";
+
+export async function getConversations({ signal, userId, pagination = {} }) {
+    const result = await fetch(`${SERVER_API_URL}/conversations/getConversations`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        signal,
+        credentials: "include",
+        body: JSON.stringify({
+            pagination,
+            userId
+        })
+    });
+
+    const responseData = await result.json();
+
+    if (result.ok) {
+        return responseData;
+    } else {
+        throw new Error(responseData.error || "Something went wrong");
+    }
+}
