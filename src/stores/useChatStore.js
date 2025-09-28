69576521c5f29237ef29e1d475e1e0fa2b1adfbb
John <johnsuyang2118@gmail.com>
2025-09-28 19:47:13 +0800

    properly update the selected convo

--- src/stores/useChatStore.js
+++ src/stores/useChatStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 export const useChatStore = create((set) => ({
     openChat: false,
     selectedConvo: undefined,
-    updateSelectedConvo: (data) => set(({ selectedConvo }) => ({ ...selectedConvo, ...data })),
+    updateSelectedConvo: (data) => set(({ selectedConvo }) => ({ selectedConvo: { ...selectedConvo, ...data } })),
     setSelectedConvo: (convo) => set(() => ({ selectedConvo: convo })),
     handleOpenChat: () => set(() => ({ openChat: true })),
     handleCloseChat: () => set(() => ({ openChat: false }))
