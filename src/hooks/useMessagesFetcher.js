ca5ade3b9a329fe3b36bd37da36f2950d83734e2
John <johnsuyang2118@gmail.com>
2025-10-08 03:39:31 +0800

    useEffectEvent to prevent uneccesary rerender

--- src/hooks/useMessagesFetcher.js
+++ src/hooks/useMessagesFetcher.js
@@ -1,6 +1,6 @@
 import { useFetcher } from "react-router";
 import { useToastStore } from "../stores/useToastStore";
-import { useEffect } from "react";
+import { useEffect, useEffectEvent } from "react";
 import { usePagination } from "./usePagination";
 import { useChatStore } from "../stores/useChatStore";
 
@@ -11,34 +11,37 @@ export function useMessagesFetcher(ref) {
     const fetcher = useFetcher();
     const createToast = useToastStore((state) => state.createToast);
 
+    const loadMessages = useEffectEvent((messages) => {
+        const lastMessage = messages[0];
+        const exist = messages.every((m) => selectedConvo.messages.find((im) => im._id == m._id));
+
+        if (!exist) {
+            setPagination((prev) => {
+                return { ...prev, cursor: lastMessage.updatedAt };
+            });
+
+            if (selectedConvo) {
+                updateSelectedConvo((prev) => ({
+                    ...prev,
+                    messages: [...messages, ...prev.messages]
+                }));
+            }
+        } else {
+            if (fetcher.state !== "idle") {
+                ref.current.scrollTop = 500;
+            }
+        }
+    });
+
     useEffect(() => {
         if (fetcher?.data?.error) {
             createToast({ headerText: "Server Error", bodyText: fetcher?.data?.error, isSuccess: false });
         }
 
         if (fetcher?.data?.result && fetcher?.data?.result.length) {
-            const messages = fetcher?.data?.result.reverse();
-            const lastMessage = messages[0];
-            const exist = messages.every((m) => selectedConvo.messages.find((im) => im._id == m._id));
-
-            if (!exist) {
-                setPagination((prev) => {
-                    return { ...prev, cursor: lastMessage.updatedAt };
-                });
-
-                if (selectedConvo) {
-                    updateSelectedConvo((prev) => ({
-                        ...prev,
-                        messages: [...messages, ...prev.messages]
-                    }));
-                }
-            } else {
-                if (fetcher.state !== "idle") {
-                    ref.current.scrollTop = 500;
-                }
-            }
+            loadMessages(fetcher?.data?.result.reverse());
         }
-    }, [fetcher, createToast, setPagination, selectedConvo, updateSelectedConvo, ref]);
+    }, [fetcher, createToast]);
 
     return { fetcher, pagination, selectedConvo };
 }
