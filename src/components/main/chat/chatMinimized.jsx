e92b3f4e1a6c5769fae7e8fda414590f6d02d1b2
John <johnsuyang2118@gmail.com>
2025-10-11 06:50:15 +0800

    fetch conversations when opening chat in small screens

--- src/components/main/chat/chatMinimized.jsx
+++ src/components/main/chat/chatMinimized.jsx
@@ -1,9 +1,81 @@
+import { useEffect, useEffectEvent, useState } from "react";
 import ChatDotsIcon from "../../../assets/icons/chatDotsIcon";
-function ChatMinimized({ chatCount, onClick, isMaxChatOpen }) {
+import { objectToFormData } from "../../../lib/utilities";
+import { useGetConversations } from "../../../hooks/useGetConversations";
+import { useMarkUnreadMessagesFetcher } from "../../../hooks/useMarkUnreadMessagesFetcher";
+import { useChatStore } from "../../../stores/useChatStore";
+import { useSlideStore } from "../../../stores/useSlideStore";
+import { useUserStore } from "../../../stores/useUserStore";
+import { SUBMENU_HEADERS } from "../../../lib/constants";
+import ChatConvo from "./chatConvo";
+
+function ChatMinimized({ chatCount, onClick }) {
+    const openSlide = useSlideStore((state) => state.openSlide);
+    const setSelectedConvo = useChatStore((state) => state.setSelectedConvo);
+    const openChatSmall = useChatStore((state) => state.openChatSmall);
+    const conversations = useChatStore((state) => state.conversations);
+    const user = useUserStore((state) => state.user);
+    const { fetcher } = useMarkUnreadMessagesFetcher();
+
+    const [scrollAtTheBottom, setScrollAtTheBottom] = useState(false);
+    const { isLoading } = useGetConversations(user, scrollAtTheBottom, true);
+
+    function handleConvoClick(convo, unreadCount) {
+        setSelectedConvo(convo);
+
+        if (unreadCount > 0) {
+            fetcher.submit(
+                objectToFormData({
+                    conversationId: convo._id,
+                    userId: user.id
+                }),
+                { action: "/chat/markUnreadMessages", method: "PUT" }
+            );
+        }
+    }
+
+    const getComponents = useEffectEvent(() => {
+        return conversations.map((convo) => {
+            const { _id, participants, updatedAt, messages } = convo;
+            const convoWith = participants.find((u) => u.userId != user.id);
+            const latestMessage = messages[messages.length - 1];
+            const text = latestMessage ? latestMessage.message : "...";
+            const unreadCount = messages.filter((m) => !m.isReadBy?.includes(user.id)).length;
+            // so that text in the convo sidebar won't change if
+            // error happened during optimistic update
+            const deepCopy = JSON.parse(JSON.stringify(convo));
+            return (
+                <ChatConvo
+                    onClick={() => handleConvoClick(deepCopy, unreadCount)}
+                    name={convoWith.name}
+                    date={updatedAt}
+                    key={_id}
+                    text={text}
+                    unreadCount={unreadCount}
+                    mobileView
+                />
+            );
+        });
+    });
+
+    useEffect(() => {
+        if (openChatSmall && conversations) {
+            const component = getComponents();
+
+            if (component.length) {
+                openSlide({
+                    open: true,
+                    header: SUBMENU_HEADERS[2],
+                    component
+                });
+            }
+        }
+    }, [openChatSmall, conversations, openSlide]);
+
     return (
         <div
             className={`chat-minimized d-none d-sm-block position-fixed bottom-0 end-0 me-3 bg-primary py-2 px-3 rounded-top text-white position-relative ${
-                !isMaxChatOpen ? "chat-minimized-open" : ""
+                openChatSmall ? "chat-minimized-open" : ""
             } `}
             onClick={onClick}
             role="button"
--- src/layout/baseHeader.jsx
+++ src/layout/baseHeader.jsx
@@ -3,30 +3,17 @@ import UserIcon from "../assets/icons/userIcon";
 import ChatDotsIcon from "../assets/icons/chatDotsIcon";
 import SearchBar from "../components/header/searchBar";
 import SignInIcon from "../assets/icons/signInIcon";
-import { SUBMENU_HEADERS } from "../lib/constants";
 import MenuDropDown from "../components/header/menuDropDown";
-import ChatConvo from "../components/main/chat/chatConvo";
 import { useUserStore } from "../stores/useUserStore";
-import { useSlideStore } from "../stores/useSlideStore";
+import { useChatStore } from "../stores/useChatStore";
 
 function BaseHeader({ chatCount = 20 }) {
-    const openSlide = useSlideStore((state) => state.openSlide);
+    const handleOpenChat = useChatStore((state) => state.handleOpenChat);
     const user = useUserStore((state) => state.user);
     const navigate = useNavigate();
 
     function handleSignIn() {
         navigate("/auth");
-    }
-
-    function handleChatClick() {
-        openSlide({
-            open: true,
-            header: SUBMENU_HEADERS[2],
-            component: [
-                <ChatConvo key={1} name="john doe doe doe" date="2022/11/17" text="very important message" convoCount={5} mobileView />,
-                <ChatConvo key={4} name="jane doe doe doe" date="2022/11/17" text="message important very very" convoCount={0} mobileView />
-            ]
-        });
     }
 
     return (
@@ -44,7 +31,7 @@ function BaseHeader({ chatCount = 20 }) {
 
                         {user?.name ? (
                             <>
-                                <div className="d-sm-none position-relative" role="button" onClick={handleChatClick}>
+                                <div className="d-sm-none position-relative" role="button" onClick={() => handleOpenChat(true)}>
                                     <ChatDotsIcon className="text-white me-2" height="20" width="20" />
                                     <span
                                         className="extra-small-text border border-light position-absolute top-0 bg-white text-secondary rounded-circle mt-n2 ms-n3 text-center"
