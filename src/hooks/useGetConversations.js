60a1b43066b61f4f463490bd1c1b6421178e397d
John <johnsuyang2118@gmail.com>
2025-10-10 08:37:54 +0800

    append or change entirely when fetching conversations

--- src/hooks/useGetConversations.js
+++ src/hooks/useGetConversations.js
@@ -7,24 +7,26 @@ import { useChatStore } from "../stores/useChatStore";
 export function useGetConversations(user, isOpen, scrollsAtBottom) {
     const { pagination, setPagination } = usePagination({ limit: 8 });
     const setConversations = useChatStore((state) => state.setConversations);
-    const conversations = useChatStore((state) => state.conversations);
 
     const isOpenAndTheresUser = isOpen && user?.id;
-    const isInitialLoad = isOpenAndTheresUser && conversations.length == 0;
     const userScrollsDown = isOpenAndTheresUser && scrollsAtBottom;
 
     const { data: { details = [] } = {}, isLoading } = useQuery({
         queryKey: ["chat", "conversations"],
         queryFn: ({ signal }) => getConversations({ signal, userId: user?.id, pagination }),
-        enabled: isInitialLoad || userScrollsDown
+        enabled: Boolean(isOpenAndTheresUser) || userScrollsDown
     });
 
     useEffect(() => {
         if (details.length && !isLoading) {
-            setConversations(details);
-            setPagination((prev) => ({ ...prev, cursor: details[0].updatedAt }));
+            if (scrollsAtBottom) {
+                setConversations((conversations) => [...conversations, ...details]);
+                setPagination((prev) => ({ ...prev, cursor: details[0].updatedAt }));
+            } else {
+                setConversations(() => details);
+            }
         }
-    }, [details, isLoading, setConversations, setPagination]);
+    }, [details, isLoading, setConversations, scrollsAtBottom, setPagination]);
 
     return { isLoading };
 }
